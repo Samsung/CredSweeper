9d39bdcea1ad4f0a86350e8072256f3579d09ad7
Alex Sokol <o.sokol@samsung.com>
2022-02-03 18:10:25 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ author = 'CredTeam'
 version = '1.2'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.0'
+release = '1.2.1'
 
 # The master toctree document.
 master_doc = 'index'
