8e243763d62ec908bb2209f7dc8f6665bc3270c0
Alex Sokol <o.sokol@samsung.com>
2022-02-10 13:15:51 +0200

    Add all the main submodules to the __init__.py

--- credsweeper/__init__.py
+++ credsweeper/__init__.py
@@ -1,3 +1,14 @@
 import os
 
+from credsweeper.app import CredSweeper
+from credsweeper.common.constants import ThresholdPreset
+from credsweeper.file_handler import ContentProvider, ByteContentProvider, DiffContentProvider, StringContentProvider, \
+    TextContentProvider
+from credsweeper.ml_model.ml_validator import MlValidator
+
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+__all__ = [
+    'ByteContentProvider', 'ContentProvider', 'DiffContentProvider', 'MlValidator', 'StringContentProvider',
+    'TextContentProvider', 'ThresholdPreset'
+]
