2f1f6fc58623ae28d58b460d88014e8a619028c8
Alex Sokol <o.sokol@samsung.com>
2022-02-10 13:14:45 +0200

    Implement new content providers and tests

---
+++ credsweeper/file_handler/__init__.py
@@ -0,0 +1,9 @@
+from credsweeper.file_handler.byte_content_provider import ByteContentProvider
+from credsweeper.file_handler.content_provider import ContentProvider
+from credsweeper.file_handler.diff_content_provider import DiffContentProvider
+from credsweeper.file_handler.string_content_provider import StringContentProvider
+from credsweeper.file_handler.text_content_provider import TextContentProvider
+
+__all__ = [
+    'ByteContentProvider', 'ContentProvider', 'DiffContentProvider', 'StringContentProvider', 'TextContentProvider'
+]
