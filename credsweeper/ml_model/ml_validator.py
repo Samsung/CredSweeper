e83f265103e21b42cebb89c485826828b8a2c1d2
Roman Babenko <babenek@gmail.com>
2022-02-16 13:30:15 +0200

    Fix name 'features' in code. Use string output type declaration

--- credsweeper/ml_model/ml_validator.py
+++ credsweeper/ml_model/ml_validator.py
@@ -2,7 +2,7 @@ import json
 import os
 import pathlib
 import string
-from typing import List, Tuple, Union, Any
+from typing import List, Tuple, Union
 
 from credsweeper.common.constants import ThresholdPreset, DEFAULT_ENCODING
 from credsweeper.credentials import Candidate
@@ -20,18 +20,9 @@ try:
 
     ML_VALIDATOR_IMPORT_ERROR = None
 except ModuleNotFoundError as e:
-    class np:
-        """
-        Dummy class to fix
-        NameError: name 'np' is not defined
-        """
-        ndarray = Any
-
-
     ML_VALIDATOR_IMPORT_ERROR = "The ML Validation function cannot be used without additional ML packages.\n" \
                                 f"{e.msg}\n" \
                                 "Run `pip install credsweeper[ml]` to fix it."
-
 
 
 class MlValidator:
@@ -42,7 +33,6 @@ class MlValidator:
             raise ModuleNotFoundError(ML_VALIDATOR_IMPORT_ERROR)
         tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)  # To make TF logger quiet
         config = tf.compat.v1.ConfigProto()
-        # pylint: disable=E1101
         config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
         config.log_device_placement = True  # to log device placement (on which device the operation ran)
         sess = tf.compat.v1.Session(config=config)
@@ -87,7 +77,7 @@ class MlValidator:
                 cls.common_feature_list.append(feature)
 
     @classmethod
-    def encode(cls, line, char_to_index) -> np.ndarray:
+    def encode(cls, line, char_to_index) -> 'np.ndarray':
         encoded = []
         for c in line.strip().lower():
             if c in char_to_index:
@@ -100,35 +90,35 @@ class MlValidator:
         return one_hot[0]
 
     @classmethod
-    def extract_common_features(cls, candidates: List[Candidate]) -> np.ndarray:
+    def extract_common_features(cls, candidates: List[Candidate]) -> 'np.ndarray':
         """Extract features that are guaranteed to be the same for all candidates on the same line with same value."""
-        features = np.array([], dtype=float)
+        np_array = np.array([], dtype=float)
         # Extract features from credential candidate
         default_candidate = candidates[0]
         for feature in cls.common_feature_list:
             new_feature = feature([default_candidate])[0]
             if not isinstance(new_feature, np.ndarray):
                 new_feature = np.array([new_feature])
-            features = np.append(features, new_feature)
-        return features
+            np_array = np.append(np_array, new_feature)
+        return np_array
 
     @classmethod
-    def extract_unique_features(cls, candidates: List[Candidate]) -> np.ndarray:
+    def extract_unique_features(cls, candidates: List[Candidate]) -> 'np.ndarray':
         """Extract features that can by different between candidates. Join them with or operator."""
-        features = np.array([], dtype=bool)
+        np_array = np.array([], dtype=bool)
         default_candidate = candidates[0]
         for feature in cls.unique_feature_list:
             new_feature = feature([default_candidate])[0]
             if not isinstance(new_feature, np.ndarray):
                 new_feature = np.array([new_feature])
-            features = np.append(features, new_feature)
+            np_array = np.append(np_array, new_feature)
         for candidate in candidates[1:]:
             for feature in cls.unique_feature_list:
                 new_feature = feature([candidate])[0]
                 if not isinstance(new_feature, np.ndarray):
                     new_feature = np.array([new_feature])
-                features = features | new_feature
-        return features
+                np_array = np_array | new_feature
+        return np_array
 
     @classmethod
     def validate(cls, candidate: Candidate) -> Tuple[bool, float]:
@@ -138,19 +128,19 @@ class MlValidator:
         return is_cred_batch[0], probability_batch[0]
 
     @classmethod
-    def get_group_features(cls, value: str, candidates: List[Candidate]) -> Tuple[np.ndarray, np.ndarray]:
+    def get_group_features(cls, value: str, candidates: List[Candidate]) -> Tuple['np.ndarray', 'np.ndarray']:
         # `np.newaxis` used to add new dimension if front, so input will be treated as a batch
         line_input = cls.encode(value, cls.char_to_index)[np.newaxis]
 
         common_features = cls.extract_common_features(candidates)
         unique_features = cls.extract_unique_features(candidates)
-        features = np.hstack([common_features, unique_features])
-        features = np.array([features])
-        return line_input, features
+        np_array = np.hstack([common_features, unique_features])
+        np_array = np.array([np_array])
+        return line_input, np_array
 
     @classmethod
     def validate_groups(cls, group_list: List[Tuple[str, List[Candidate]]],
-                        batch_size: int) -> Tuple[np.ndarray, np.ndarray]:
+                        batch_size: int) -> Tuple['np.ndarray', 'np.ndarray']:
         """Use ml model on list of candidate groups.
 
         Args:
@@ -165,17 +155,17 @@ class MlValidator:
         line_input_list = []
         features_list = []
         for (value, candidates) in group_list:
-            line_input, features = cls.get_group_features(value, candidates)
+            line_input, np_array = cls.get_group_features(value, candidates)
             line_input_list.append(line_input)
-            features_list.append(features)
+            features_list.append(np_array)
 
         probability = np.zeros(len(features_list))
         for i in range(0, len(features_list), batch_size):
             line_inputs = line_input_list[i:i + batch_size]
             line_inputs = np.vstack(line_inputs)
-            features = features_list[i:i + batch_size]
-            features = np.vstack(features)
-            probability[i:i + batch_size] = cls.model([line_inputs, features])[:, 0]
+            np_array = features_list[i:i + batch_size]
+            np_array = np.vstack(np_array)
+            probability[i:i + batch_size] = cls.model([line_inputs, np_array])[:, 0]
         is_cred = probability > cls.threshold
         for i in range(len(is_cred)):
             logging.debug(
