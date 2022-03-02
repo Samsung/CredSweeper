45ad6207b33523ab811c82f8a14ce19f5c370919
Myoungdo Park <cuspymd@gmail.com>
2022-03-02 23:12:33 +0900

    Fix unused import error

--- credsweeper/file_handler/diff_content_provider.py
+++ credsweeper/file_handler/diff_content_provider.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Tuple
+from typing import List, Tuple
 
 from credsweeper.file_handler.analysis_target import AnalysisTarget
 from credsweeper.file_handler.content_provider import ContentProvider
