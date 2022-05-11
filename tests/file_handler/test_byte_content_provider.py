928006f692b4f72f64582031650aef6372403d35
Roman Babenko <babenek@gmail.com>
2022-05-11 17:14:58 +0300

    Added indicator test

--- tests/file_handler/test_byte_content_provider.py
+++ tests/file_handler/test_byte_content_provider.py
@@ -1,9 +1,11 @@
+import os
 from typing import List
 
 import pytest
 
 from credsweeper.file_handler.analysis_target import AnalysisTarget
 from credsweeper.file_handler.byte_content_provider import ByteContentProvider
+from credsweeper.utils import Util
 
 
 class TestByteContentProvider:
@@ -21,3 +23,19 @@ class TestByteContentProvider:
 
         target = analysis_targets[0]
         assert target == expected_target
+
+    def test_byte_content_provider_p(self) -> None:
+        files_counter = 0
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        tests_path = os.path.join(dir_path, "..", "samples")
+        for dir_path, _, filenames in os.walk(tests_path):
+            filenames.sort()
+            for filename in filenames:
+                files_counter += 1
+                file_path = os.path.join(dir_path, filename)
+                util_text = Util.read_file(file_path)
+                with open(file_path, 'rb') as f:
+                    bin_data = f.read()
+                provider = ByteContentProvider(bin_data)
+                assert util_text == provider.lines
+        assert files_counter == 39
