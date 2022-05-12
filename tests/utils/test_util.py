1f73718eee7abe939d6bfd673a7a4eb1116c57ad
Roman Babenko <babenek@users.noreply.github.com>
2022-05-12 17:36:35 +0300

    Update tests/utils/test_util.py

--- tests/utils/test_util.py
+++ tests/utils/test_util.py
@@ -59,7 +59,6 @@ class TestUtils:
                 if bin_char != encoded_bin:
                     raise Exception(f"Wrong refurbish:{utf8_char} {bin_char} {encoded_bin}")
             except Exception as exc:
-                # print(f'{exc}, {bin_char}')
                 continue
             # the byte sequence is correct for UTF-8 and is added to data
             bin_text += bin_char
