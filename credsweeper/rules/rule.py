c3cf3ba82395cccbdf4f2a3f8a09de8a4150e288
Roman Babenko <babenek@gmail.com>
2022-06-13 20:44:07 +0300

    Use static functions instead unclear setters

--- credsweeper/rules/rule.py
+++ credsweeper/rules/rule.py
@@ -40,11 +40,14 @@ class Rule:
         self.config = config
         self._assert_all_rule_fields(rule_template)
         self.rule_name: Optional[str] = rule_template["name"]
-        self.rule_type: Optional[RuleType] = getattr(RuleType, rule_template["type"].upper(), None)
+        _rule_template_type = rule_template["type"]
+        self.rule_type: Optional[RuleType] = getattr(RuleType, _rule_template_type.upper(), None)
+        if self.rule_type is None:
+            raise ValueError(f"Malformed rule config file. Rule type '{_rule_template_type}' is invalid.")
         self.severity: Severity = rule_template["severity"]
         self.filters: List[Filter] = rule_template.get("filter_type")
-        self.patterns: List[regex.Pattern] = (rule_template["type"], rule_template["values"])
-        self.pattern_type: Optional[str] = (rule_template["type"], rule_template["values"])
+        self.patterns: List[regex.Pattern] = Rule._get_patterns(self.rule_type, rule_template["values"])
+        self.pattern_type: str = Rule._get_pattern_type(self.rule_type, len(self.patterns))
         self.use_ml: bool = rule_template["use_ml"]
         self.validations: List[Validation] = rule_template.get("validations")
         self.required_substrings: List[str] = [s.lower() for s in rule_template.get("required_substrings", [""])]
@@ -91,13 +94,9 @@ class Rule:
                 raise ValueError(f'Malformed rule config file. Rule filter_type "{filter_type}" is invalid.')
             self.__filters = filter_group(self.config).filters
 
-    @property
-    def patterns(self) -> List[regex.Pattern]:
-        return self.__patterns
-
-    @patterns.setter
-    def patterns(self, args: Tuple[str, List[str]]) -> None:
-        """Set pattern value for rule object.
+    @staticmethod
+    def _get_patterns(_rule_type: RuleType, _values: List[str]) -> List[regex.Pattern]:
+        """Get pattern values for rule object.
 
         Set the pattern value attribute of the rule object based on the passed values.
         So, if the received rule type corresponds to the RuleType.KEYWORD type,
@@ -107,27 +106,34 @@ class Rule:
         assigned the compile regex ov received value
 
         Args:
-            args: Tuple of rule type and regular expressions
+            _rule_type: type of rule
+            _values: regular expressions
 
         """
-        rule_type_str, values = args
-        self.__patterns = []
-        if self.rule_type is None:
-            raise ValueError(f'Malformed rule config file. Rule type "{rule_type_str}" is invalid.')
-        if self.rule_type == RuleType.KEYWORD:
-            for value in values:
-                self.__patterns.append(Util.get_keyword_pattern(value))
-        elif self.rule_type in (RuleType.PATTERN, RuleType.PEM_KEY):
-            for value in values:
-                self.__patterns.append(regex.compile(value))
+        _patterns = []
+        if RuleType.KEYWORD == _rule_type:
+            for value in _values:
+                _patterns.append(Util.get_keyword_pattern(value))
+        elif _rule_type in (RuleType.PATTERN, RuleType.PEM_KEY):
+            for value in _values:
+                _patterns.append(regex.compile(value))
+        else:
+            raise ValueError(f"Malformed rule config file. Rule type '{_rule_type}' is invalid.")
+        return _patterns
 
     @property
-    def pattern_type(self) -> str:
-        return self.__pattern_type
+    def patterns(self) -> List[regex.Pattern]:
+        """patterns getter"""
+        return self.__patterns
 
-    @pattern_type.setter
-    def pattern_type(self, args: Tuple[str, List[str]]) -> None:
-        """Set pattern type for rule object.
+    @patterns.setter
+    def patterns(self, _patterns: List[regex.Pattern]) -> None:
+        """patterns setter"""
+        self.__patterns = _patterns
+
+    @staticmethod
+    def _get_pattern_type(_rule_type: RuleType, _values_len: int) -> str:
+        """Detect pattern type for rule object.
 
         Set the pattern_type attribute of the rule object based on the passed values.
         So, if the received rule type corresponds to the RuleType.PEM_KEY type,
@@ -136,19 +142,30 @@ class Rule:
         and for rules with more than one value set "multi_pattern" type
 
         Args:
-            args: Tuple of rule type and regular expressions
+            _rule_type: rule type
+            _values_len: length of values with expressions
 
         """
-        rule_type_str, values = args
-        self.__pattern_type = None
-        if self.rule_type is None:
-            raise ValueError(f'Malformed rule config file. Rule type "{rule_type_str}" is invalid.')
-        if self.rule_type == RuleType.PEM_KEY:
-            self.__pattern_type = self.PEM_KEY_PATTERN
-        elif len(values) == 1:
-            self.__pattern_type = self.SINGLE_PATTERN
-        elif len(values) > 1:
-            self.__pattern_type = self.MULTI_PATTERN
+        _pattern_type: Optional[str] = None
+        if RuleType.PEM_KEY == _rule_type:
+            _pattern_type = Rule.PEM_KEY_PATTERN
+        elif 1 == _values_len:
+            _pattern_type = Rule.SINGLE_PATTERN
+        elif 1 < _values_len:
+            _pattern_type = Rule.MULTI_PATTERN
+        else:
+            raise ValueError(f"Malformed rule config file. Rule type '{_rule_type}' or '{_values_len}' are invalid.")
+        return _pattern_type
+
+    @property
+    def pattern_type(self) -> str:
+        """pattern_type getter"""
+        return self.__pattern_type
+
+    @pattern_type.setter
+    def pattern_type(self, _pattern_type: str) -> None:
+        """pattern_type setter"""
+        self.__pattern_type = _pattern_type
 
     @property
     def use_ml(self) -> bool:
