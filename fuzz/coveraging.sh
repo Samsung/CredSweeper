92322763ca017fee33a21c6233bc88cb4ecb0494
Roman Babenko <babenek@gmail.com>
2022-06-15 08:49:37 +0300

    fuzzed

--- fuzz/coveraging.sh
+++ fuzz/coveraging.sh
@@ -6,11 +6,16 @@ set -e
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 cd "${THISDIR}/.."
 
-python -m fuzz \
+rm -vf .coverage
+
+python -m coverage run \
+    --source=credsweeper \
+    fuzz \
     -max_len=1024 \
-    -rss_limit_mb=6000 \
-    -seed=$(date +%s) \
-    -atheris_runs=$(( 1024 + $(ls corpus | wc -l) )) \
+    -rss_limit_mb=2048 \
+    -atheris_runs=$(ls corpus | wc -l) \
     -verbosity=1 \
     corpus/ \
     ;
+
+python -m coverage report
