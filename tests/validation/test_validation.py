55fd5f3ed13b42fbec3fc4724f1e94a885144dc9
Roman Babenko <babenek@gmail.com>
2022-06-16 17:29:46 +0300

    Test discovers failure in wrong server response

---
+++ tests/validation/test_validation.py
@@ -0,0 +1,54 @@
+from unittest import mock
+
+import pytest
+import regex
+from requests import Response
+
+from credsweeper.common.constants import Severity, KeyValidationOption
+from credsweeper.credentials import Candidate, LineData
+from credsweeper.validations import \
+    GithubTokenValidation, \
+    GoogleApiKeyValidation, \
+    StripeApiKeyValidation, \
+    GoogleMultiValidation, \
+    SquareClientIdValidation, \
+    SquareAccessTokenValidation, \
+    SlackTokenValidation, \
+    MailChimpKeyValidation
+from credsweeper.validations.apply_validation import ApplyValidation
+
+
+def mocked_requests_get(*args, **kwargs):
+    response = Response()
+    response.status_code = 200
+    response._content = b''
+    return response
+
+
+@mock.patch('requests.get', mock.Mock(side_effect=mocked_requests_get))
+@pytest.mark.parametrize("validator", [  #
+    GithubTokenValidation,  #
+    GoogleApiKeyValidation,  #
+    GoogleMultiValidation,  #
+    MailChimpKeyValidation,  #
+    SlackTokenValidation,  #
+    SquareAccessTokenValidation,  #
+    SquareClientIdValidation,  #
+    StripeApiKeyValidation])
+def test_mocked_validation_n(validator):
+    candidate = Candidate(
+        line_data_list=[  #
+            LineData({}, line="dummy line 1", line_num=1, path="dummy path 1", pattern=regex.compile('.*')),
+            LineData({}, line="dummy line 2", line_num=2, path="dummy path 2", pattern=regex.compile('.*'))
+        ],
+        patterns=[regex.compile('.*')], rule_name="Dummy candidate", severity=Severity.INFO, config={},
+        validations=[validator])
+
+    assert candidate.api_validation == KeyValidationOption.NOT_AVAILABLE
+
+    apply_validation = ApplyValidation()
+    assert apply_validation.validate(candidate) in [  #
+        KeyValidationOption.VALIDATED_KEY,  #
+        KeyValidationOption.UNDECIDED,  #
+        KeyValidationOption.INVALID_KEY,  #
+        KeyValidationOption.NOT_AVAILABLE]
