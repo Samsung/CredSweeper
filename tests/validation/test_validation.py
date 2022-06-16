fd83359a02d471c0eeab7bd89832871bf1a25dd6
Roman Babenko <babenek@gmail.com>
2022-06-16 18:22:44 +0300

    Reform style

--- tests/validation/test_validation.py
+++ tests/validation/test_validation.py
@@ -26,22 +26,28 @@ def mocked_requests_get(*args, **kwargs):
 
 
 @mock.patch('requests.get', mock.Mock(side_effect=mocked_requests_get))
-@pytest.mark.parametrize("validator", [  #
-    GithubTokenValidation,  #
-    GoogleApiKeyValidation,  #
-    GoogleMultiValidation,  #
-    MailChimpKeyValidation,  #
-    SlackTokenValidation,  #
-    SquareAccessTokenValidation,  #
-    SquareClientIdValidation,  #
-    StripeApiKeyValidation])
+@pytest.mark.parametrize(  #
+    "validator",  #
+    [  #
+        GithubTokenValidation,  #
+        GoogleApiKeyValidation,  #
+        GoogleMultiValidation,  #
+        MailChimpKeyValidation,  #
+        SlackTokenValidation,  #
+        SquareAccessTokenValidation,  #
+        SquareClientIdValidation,  #
+        StripeApiKeyValidation  #
+    ])
 def test_mocked_validation_n(validator):
     candidate = Candidate(
         line_data_list=[  #
             LineData({}, line="dummy line 1", line_num=1, path="dummy path 1", pattern=regex.compile('.*')),
             LineData({}, line="dummy line 2", line_num=2, path="dummy path 2", pattern=regex.compile('.*'))
         ],
-        patterns=[regex.compile('.*')], rule_name="Dummy candidate", severity=Severity.INFO, config={},
+        patterns=[regex.compile('.*')],  #
+        rule_name="Dummy candidate",  #
+        severity=Severity.INFO,  #
+        config={},  #
         validations=[validator])
     candidate.line_data_list[0].value = "-"
 
@@ -52,4 +58,5 @@ def test_mocked_validation_n(validator):
         KeyValidationOption.VALIDATED_KEY,  #
         KeyValidationOption.UNDECIDED,  #
         KeyValidationOption.INVALID_KEY,  #
-        KeyValidationOption.NOT_AVAILABLE]
+        KeyValidationOption.NOT_AVAILABLE  #
+    ]
