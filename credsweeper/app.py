fba3f270768520dd5eb20f523ed00a10e2602103
sh519-choi <sh519.choi@samsung.com>
2022-07-19 10:47:00 +0900

    Move pandas import for pylint

--- credsweeper/app.py
+++ credsweeper/app.py
@@ -4,10 +4,10 @@ import itertools
 import json
 import multiprocessing
 import os
-import pandas as pd  # type: ignore  # to avoid mypy error
 import signal
 import sys
 import zipfile
+import pandas as pd  # type: ignore  # to avoid mypy error
 
 from credsweeper.common.constants import KeyValidationOption, ThresholdPreset, DEFAULT_ENCODING, \
     RECURSIVE_SCAN_LIMITATION
