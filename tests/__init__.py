62f2d3c21546f0f216a926197c74daa896170793
Roman Babenko <babenek@gmail.com>
2022-07-25 17:43:42 +0300

    Fix and test AWS multi pattern

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,11 @@
 # total number of files in test samples, included .gitignore
-SAMPLES_FILES_COUNT: int = 43
+SAMPLES_FILES_COUNT: int = 44
 
 # credentials count after scan
-SAMPLES_CRED_COUNT: int = 50
+SAMPLES_CRED_COUNT: int = 51
 
 # credentials count after post-processing
-SAMPLES_POST_CRED_COUNT: int = 18
+SAMPLES_POST_CRED_COUNT: int = 19
 
 # well known string with all latin letters
 AZ_DATA = b"The quick brown fox jumps over the lazy dog"
