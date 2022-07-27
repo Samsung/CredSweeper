7ce970cd6278bc43e666bd72cafb90b5cf97cd16
Roman Babenko <babenek@gmail.com>
2022-07-27 15:41:57 +0300

    New rule for AWS ID

--- tests/common/test_regex.py
+++ tests/common/test_regex.py
@@ -4,25 +4,35 @@ from regex import regex
 
 class TestRegex:
     TEST_AWS_ID = "ASCA0123456789ABCDEF"
-    TEST_AWS_REGEX = regex.compile("(^|(?:[^0-9A-Za-z]))(?P<value>(ASCA|ASIA)[0-9A-Z]{16})((?:[^0-9A-Za-z])|$)")
+    TEST_AWS_REGEX = regex.compile(
+        "(^|[^0-9A-Za-z])(?P<value>(AROA|ASCA)[0-9A-Z]{16})([0-9A-Z]?)([^0-9A-Za-z]|$)")
 
-    @pytest.mark.parametrize('text', [  #
-        f"A{TEST_AWS_ID}X",  #
-        f"{TEST_AWS_ID}X",  #
-        f"A{TEST_AWS_ID}",  #
-        f"SCA0123456789012345",  #
-        f"ASCA012345678901234"  #
-    ])
+    @pytest.mark.parametrize(  #
+        'text',  #
+        [  #
+            f"{TEST_AWS_ID}XX",  # 22 symbols
+            f"i{TEST_AWS_ID}",  #
+            f"{TEST_AWS_ID[0:18]}",  #
+            f"{TEST_AWS_ID[0:18]}x",  #
+            f"{TEST_AWS_ID[0:18]}+",  #
+            f"{TEST_AWS_ID[0:18]}/",  #
+            f"{TEST_AWS_ID[1:19]}"  #
+        ])
     def test_regex_n(self, text: str):
         assert TestRegex.TEST_AWS_REGEX.search(text) is None
 
-    @pytest.mark.parametrize('text', [  #
-        f"{TEST_AWS_ID}",  #
-        f"N={TEST_AWS_ID}",  #
-        f"\"{TEST_AWS_ID}\"",  #
-        f"{TEST_AWS_ID}/6/g'",  #
-        f"sed 's/{TEST_AWS_ID}/6/g'"  #
-    ])
+    @pytest.mark.parametrize(  #
+        'text',  #
+        [  #
+            f"{TEST_AWS_ID}",  #
+            f"N={TEST_AWS_ID}",  #
+            f"\"{TEST_AWS_ID}\"",  #
+            f"{TEST_AWS_ID}/6/g'",  #
+            f"={TEST_AWS_ID}%2F",  #
+            f"sed 's/{TEST_AWS_ID}/6/g'",  #
+            f"{TEST_AWS_ID}X",  #
+            f"--key {TEST_AWS_ID}X --help"  #
+        ])
     def test_regex_p(self, text: str):
         obtained = TestRegex.TEST_AWS_REGEX.search(text)
         assert obtained is not None
