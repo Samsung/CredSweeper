1f5de7fcc57f5edf85307009106a8fb50332e902
Roman Babenko <babenek@gmail.com>
2022-07-27 17:47:33 +0300

    Moved corpus to fuzz directory

--- fuzz/minimizing.sh
+++ fuzz/minimizing.sh
@@ -6,15 +6,16 @@ set -e
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 cd "${THISDIR}/.."
 
-MINDIR=.minimizing
+CORPUS_DIR=fuzz/corpus
+MINIMIZING_DIR=fuzz/.corpus.minimizing
 
-rm -vrf $MINDIR
+rm -vrf ${MINIMIZING_DIR}
 
-mkdir -vp $MINDIR
+mkdir -vp ${MINIMIZING_DIR}
 
 # ## freeze original coverage
 
-rm -rf $MINDIR/htmlcov
+rm -rf ${MINIMIZING_DIR}/htmlcov
 
 rm -vf .coverage
 
@@ -25,23 +26,23 @@ python -m coverage run \
     --source=credsweeper \
     fuzz \
     -rss_limit_mb=2048 \
-    -atheris_runs=$(( 1 + $(ls corpus | wc -l) )) \
+    -atheris_runs=$(( 1 + $(ls ${CORPUS_DIR} | wc -l) )) \
     -verbosity=1 \
-    corpus/ \
+    ${CORPUS_DIR}/ \
     ;
 
 original_cov="$(python -m coverage report | tail -1)"
 
 python -m coverage html
 
-mv htmlcov $MINDIR
+mv htmlcov ${MINIMIZING_DIR}
 
 # ## run minimization for all corpuses
 
 declare -a CORPUS
 
 i=0
-for f in $(ls corpus | shuf); do
+for f in $(ls ${CORPUS_DIR} | shuf); do
     CORPUS[$i]+=$f
     i=$(( 1 + $i ))
 done
@@ -49,27 +50,27 @@ done
 for f in ${CORPUS[@]}; do
 
     echo "test $f"
-    mkdir -vp $MINDIR/$f
-    mv -vf corpus/$f $MINDIR/$f/
+    mkdir -vp ${MINIMIZING_DIR}/$f
+    mv -vf ${CORPUS_DIR}/$f ${MINIMIZING_DIR}/$f/
 
     python -m coverage run \
         --source=credsweeper \
         fuzz \
         -rss_limit_mb=2048 \
-        -atheris_runs=$(( 1 + $(ls corpus | wc -l) )) \
+        -atheris_runs=$(( 1 + $(ls ${CORPUS_DIR} | wc -l) )) \
         -verbosity=1 \
-        corpus/ \
+        ${CORPUS_DIR}/ \
         ;
 
     python -m coverage html
-    mv htmlcov $MINDIR/$f/
-    python -m coverage report >$MINDIR/$f/report.txt
-    test_cov="$(tail -1 $MINDIR/$f/report.txt)"
+    mv htmlcov ${MINIMIZING_DIR}/$f/
+    python -m coverage report >${MINIMIZING_DIR}/$f/report.txt
+    test_cov="$(tail -1 ${MINIMIZING_DIR}/$f/report.txt)"
     if [ "$test_cov" != "$original_cov" ]; then
-        echo "corpus $f impacts on coverage"
-        cp -v $MINDIR/$f/$f corpus/
+        echo "seed file $f impacts on coverage"
+        cp -v ${MINIMIZING_DIR}/$f/$f ${CORPUS_DIR}/
     else
-        echo "corpus $f does not impact on coverage"
+        echo "seed file $f does not impact on coverage"
     fi
 
 done
