0f4c92b85a1a6c875af115d3cbf4b2fecb99f142
Roman Babenko <babenek@gmail.com>
2022-08-09 12:21:42 +0300

    swap _n and _p tests

--- tests/validation/test_square_client_id_validation.py
+++ tests/validation/test_square_client_id_validation.py
@@ -22,16 +22,16 @@ class TestSquareClientIdValidation:
         line_data_list.append(line_data)
         return line_data_list
 
-    def test_verify_n(self, line_data_list: pytest.fixture) -> None:
-        response = Response()
-        response._content = b"Unable to find client by that `client_id`"
-        with patch(requests.__name__ + "." + requests.get.__name__, return_value=response):
-            validation_result = SquareClientIdValidation.verify(line_data_list)
-        assert validation_result is KeyValidationOption.INVALID_KEY
-
     def test_verify_p(self, line_data_list: pytest.fixture) -> None:
         response = Response()
         response._content = b"<body>You are being <a>redirected"
         with patch(requests.__name__ + "." + requests.get.__name__, return_value=response):
             validation_result = SquareClientIdValidation.verify(line_data_list)
         assert validation_result is KeyValidationOption.VALIDATED_KEY
+
+    def test_verify_n(self, line_data_list: pytest.fixture) -> None:
+        response = Response()
+        response._content = b"Unable to find client by that `client_id`"
+        with patch(requests.__name__ + "." + requests.get.__name__, return_value=response):
+            validation_result = SquareClientIdValidation.verify(line_data_list)
+        assert validation_result is KeyValidationOption.INVALID_KEY
