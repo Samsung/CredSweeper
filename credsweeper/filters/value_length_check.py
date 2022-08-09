69c0a00f9062f9dfe83701e8f18a725598b5fb1d
Alex Sokol <o.sokol@samsung.com>
2022-08-09 12:08:17 +0300

    Use config min value len in ValueLengthCheck

--- credsweeper/filters/value_length_check.py
+++ credsweeper/filters/value_length_check.py
@@ -5,6 +5,9 @@ from credsweeper.filters import Filter
 class ValueLengthCheck(Filter):
     """Check if potential candidate value is not too short (longer than 4)."""
 
+    def __init__(self, min_len) -> None:
+        self.min_len = min_len
+
     def run(self, line_data: LineData) -> bool:
         """Run filter checks on received credential candidate data 'line_data'.
 
@@ -17,6 +20,6 @@ class ValueLengthCheck(Filter):
         """
         if line_data.value is None:
             return True
-        if len(line_data.value) < 4:
+        if len(line_data.value) < self.min_len:
             return True
         return False
