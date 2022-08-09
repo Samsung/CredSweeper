6782977a4d0a00b3e4315afeb09d4a6fa28c10e5
Roman Babenko <babenek@gmail.com>
2022-08-09 11:17:45 +0300

    Mocking UT which use service with ban

--- tests/validation/test_square_access_token_validation.py
+++ tests/validation/test_square_access_token_validation.py
@@ -1,7 +1,9 @@
-from os import environ
 from typing import List
+from unittest.mock import patch
 
 import pytest
+import requests
+from requests import Response
 
 from credsweeper.common.constants import KeyValidationOption
 from credsweeper.credentials import LineData
@@ -20,8 +22,16 @@ class TestSquareAccessTokenValidation:
         line_data_list.append(line_data)
         return line_data_list
 
-    @pytest.mark.skipif(environ.get("CIRCLE_PROJECT_USERNAME") is not None,
-                        reason="Server blocking requests from CI server")
     def test_verify_n(self, line_data_list: pytest.fixture) -> None:
-        validation_result = SquareAccessTokenValidation.verify(line_data_list)
+        response = Response()
+        response.status_code = 401
+        with patch(requests.__name__ + "." + requests.post.__name__, return_value=response):
+            validation_result = SquareAccessTokenValidation.verify(line_data_list)
+        assert validation_result is KeyValidationOption.INVALID_KEY
+
+    def test_verify_p(self, line_data_list: pytest.fixture) -> None:
+        response = Response()
+        response.status_code = 400
+        with patch(requests.__name__ + "." + requests.post.__name__, return_value=response):
+            validation_result = SquareAccessTokenValidation.verify(line_data_list)
         assert validation_result is KeyValidationOption.INVALID_KEY
