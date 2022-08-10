50b67ba1b355799aef7b53b8d350c4d5f3aef73c
Roman Babenko <babenek@gmail.com>
2022-08-10 15:59:49 +0300

    test fix

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 # total number of files in test samples, included .gitignore
-SAMPLES_FILES_COUNT: int = 46
+SAMPLES_FILES_COUNT: int = 47
 
 # credentials count after scan
 SAMPLES_CRED_COUNT: int = 48
