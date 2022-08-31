a8739ae5d74d95d5a8c43d293ac7e00c81a7e5f1
Roman Babenko <babenek@gmail.com>
2022-08-31 13:17:48 +0300

    Test to disclosure an issue

--- tests/filters/test_value_dictionary_keyword_check.py
+++ tests/filters/test_value_dictionary_keyword_check.py
@@ -12,11 +12,16 @@ class TestValueDictionaryKeywordCheck:
         line_data = get_line_data(file_path, line=success_line, pattern=r"(?P<value>.*$)")
         assert ValueDictionaryKeywordCheck().run(line_data) is False
 
-    @pytest.mark.parametrize("line", ["abstract123"])
+    @pytest.mark.parametrize("line", ["typically", "password"])
     def test_value_dictionary_keyword_check_n(self, file_path: pytest.fixture, line: str) -> None:
         KeywordChecklist()
         line_data = get_line_data(file_path, line=line, pattern=r"(?P<value>.*$)")
         assert ValueDictionaryKeywordCheck().run(line_data) is True
+
+    @pytest.mark.parametrize("line", ["$e<Ret&kEy!", "23aWs#uh3"])
+    def test_value_dictionary_keyword_check2_n(self, file_path: pytest.fixture, line: str) -> None:
+        line_data = get_line_data(file_path, line=line, pattern=r"(?P<value>.*$)")
+        assert ValueDictionaryKeywordCheck().run(line_data) is False
 
     def test_value_dictionary_keyword_check_none_value_n(self, file_path: pytest.fixture,
                                                          success_line: pytest.fixture) -> None:
