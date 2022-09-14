695d02375d8d03fb35c9a218a9916de5c62d08ed
sh519-choi <sh519.choi@samsung.com>
2022-09-14 18:26:09 +0900

    Update KeywordPattern to more accuracy and cover "ENC(" case

--- credsweeper/common/constants.py
+++ credsweeper/common/constants.py
@@ -4,7 +4,7 @@ from enum import Enum
 class KeywordPattern:
     """Pattern set of keyword types"""
     key = r"(?P<variable>((('|\"|`)[^:='\"`<>]*|[^:='\"`<>\s\(]*)(?P<keyword>{})[^:='\"`<>]*)('|\"|`)?)"
-    separator = r"\s*\]?\s*(?P<separator>{})((\s|\w)*\((\s|\w|=|\()*|\s*)"
+    separator = r"\s*\]?\s*(?P<separator>{})((?!\s*ENC)(\s|\w)*\((\s|\w|=|\()*|\s*)"
     value = r"(?P<value_leftquote>(\\)*(b|r)?('|\"|`))?" \
             r"(?P<value>[^'\"`(\\\')(\\\")]{0,1000})(?P<value_rightquote>(\\)*('|\"|`))?"
 
