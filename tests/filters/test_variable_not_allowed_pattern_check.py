34bb3a96e18dff5ff8a872309c2e980beae47103
sh519-choi <sh519.choi@samsung.com>
2022-10-14 14:59:26 +0900

    Add public and pubkey to test_variable_check_n() and rename the class

--- tests/filters/test_variable_not_allowed_pattern_check.py
+++ tests/filters/test_variable_not_allowed_pattern_check.py
@@ -4,14 +4,16 @@ from credsweeper.filters import VariableNotAllowedPatternCheck
 from tests.test_utils.dummy_line_data import get_line_data
 
 
-class TestVariableCheck:
+class TestVariableNotAllowedPatternCheck:
 
     def test_variable_check_p(self, file_path: pytest.fixture, success_line: pytest.fixture) -> None:
         line_data = get_line_data(file_path, line=success_line, pattern=r"(?P<variable>.*$)")
         assert VariableNotAllowedPatternCheck().run(line_data) is False
 
-    @pytest.mark.parametrize("line",
-                             ["<crackle>", "{{crackle}}", "@GIREOGI", "! ", "> ", "< ", "+ ", "* ", "/ ", "^ ", "|"])
+    @pytest.mark.parametrize("line", [
+        "<crackle>", "{{crackle}}", "@GIREOGI", "! ", "> ", "< ", "+ ", "* ", "/ ", "^ ", "|", "sharing_public_key",
+        "my_pubkey"
+    ])
     def test_variable_check_n(self, file_path: pytest.fixture, line: str) -> None:
         line_data = get_line_data(file_path, line=line, pattern=r"(?P<variable>.*$)")
         assert VariableNotAllowedPatternCheck().run(line_data) is True
