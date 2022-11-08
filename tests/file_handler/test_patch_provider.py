57f2da3a5377d79746966b4aaf23e658599d547d
Roman Babenko <babenek@gmail.com>
2022-11-08 03:46:20 +0200

    Fix windows tests

--- tests/file_handler/test_patch_provider.py
+++ tests/file_handler/test_patch_provider.py
@@ -113,13 +113,14 @@ class TestPatchProvider:
 
     def test_oversize_n(self, config: Config) -> None:
         """Evaluate warning occurrence while load oversize diff file"""
-        file_path = SAMPLES_DIR / "password.patch"
+        # use UTF-16 encoding to prevent any Windows style transformation
+        file_path = SAMPLES_DIR / "password_utf16.patch"
         patch_provider = PatchProvider([str(file_path)], "added")
 
         config.size_limit = 0
         with patch('logging.Logger.warning') as mocked_logger:
             raw_patches = patch_provider.load_patch_data(config)
-            warning_message = f"Size (232) of the file '{file_path}' is over limit (0)"
+            warning_message = f"Size (512) of the file '{file_path}' is over limit (0)"
             mocked_logger.assert_called_with(warning_message)
 
         assert isinstance(raw_patches, list)
