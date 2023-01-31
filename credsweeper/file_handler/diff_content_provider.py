93ab02099219c5ceb68d340109d21b3b81c35025
Roman Babenko <babenek@users.noreply.github.com>
2023-01-31 14:14:51 +0200

    Support binary patch with whatthepatch v1.0.4 (#255)
    * Support binary patch with whatthepatch v1.0.4
    
    * Added docstring for new function
    
    * YAPF style fix
    
    * Use --size_limit option for recursive limit size
    
    * comments to fix yapf
    
    * re-fuzzed with DiffContentProvider
    
    * Tests fix

--- credsweeper/file_handler/diff_content_provider.py
+++ credsweeper/file_handler/diff_content_provider.py
@@ -1,8 +1,12 @@
+import logging
 from typing import List, Tuple
 
+from credsweeper.common.constants import DiffRowType
 from credsweeper.file_handler.analysis_target import AnalysisTarget
 from credsweeper.file_handler.content_provider import ContentProvider
 from credsweeper.utils import DiffRowData, Util, DiffDict
+
+logger = logging.getLogger(__name__)
 
 
 class DiffContentProvider(ContentProvider):
@@ -25,9 +29,9 @@ class DiffContentProvider(ContentProvider):
     def __init__(
             self,  #
             file_path: str,  #
-            change_type: str,  #
+            change_type: DiffRowType,  #
             diff: List[DiffDict]) -> None:
-        super().__init__(file_path=file_path, info=change_type)
+        super().__init__(file_path=file_path, info=change_type.value)
         self.change_type = change_type
         self.diff = diff
 
@@ -45,11 +49,13 @@ class DiffContentProvider(ContentProvider):
             in original order(replaced all lines not mentioned in diff file with blank line)
 
         """
-        max_line_numbs = max(x.line_numb for x in lines_data)
+        max_line_numbs = max(x.line_numb for x in lines_data) if lines_data else 0
+        # fix case when whatthepatch parses wrong patch - some exceptions are possibly
+        max_line_numbs = max(max_line_numbs, len(lines_data))
         all_lines = [""] * max_line_numbs
         change_numbs = []
         for line_data in lines_data:
-            if line_data.line_type.startswith(self.change_type):
+            if line_data.line_type.value.startswith(self.change_type.value):
                 all_lines[line_data.line_numb - 1] = line_data.line
             if line_data.line_type == self.change_type:
                 change_numbs.append(line_data.line_numb)
@@ -63,8 +69,18 @@ class DiffContentProvider(ContentProvider):
 
         """
         lines_data = Util.preprocess_file_diff(self.diff)
-        change_numbs, all_lines = self.parse_lines_data(lines_data)
-        return [
-            AnalysisTarget(all_lines[l_numb - 1], l_numb, all_lines, self.file_path, self.file_type, self.change_type)
-            for l_numb in change_numbs
-        ]
+        try:
+            change_numbs, all_lines = self.parse_lines_data(lines_data)
+            return [
+                AnalysisTarget(
+                    all_lines[l_numb - 1],  #
+                    l_numb,  #
+                    all_lines,  #
+                    self.file_path,  #
+                    self.file_type,  #
+                    self.change_type.value)  #
+                for l_numb in change_numbs
+            ]
+        except OverflowError as exc:
+            logger.exception(exc)
+        return []
