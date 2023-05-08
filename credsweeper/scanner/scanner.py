85f7d6d12fd4a702f0a4d2644abb6db0a5d24e6f
Roman Babenko <babenek@gmail.com>
2023-05-08 15:40:11 +0300

    rollback

--- credsweeper/scanner/scanner.py
+++ credsweeper/scanner/scanner.py
@@ -125,16 +125,18 @@ class Scanner:
             return credentials
         keyword_targets, pattern_targets, pem_targets = self._select_and_group_targets(targets)
         for rule in self.rules:
+            min_line_len = rule.min_line_len
+            required_substrings = rule.required_substrings
             scanner = self.__scanner_for_rule[rule.rule_name]
             to_check = self.get_targets_to_check(keyword_targets, pattern_targets, pem_targets, rule)
             # It is almost two times faster to pre-compute values related to target_line than to compute them in
             # each iteration
             for target, target_line_trimmed_lower, target_line_trimmed_len in to_check:
-                if target_line_trimmed_len < rule.min_line_len:
+                if target_line_trimmed_len < min_line_len:
                     continue
                 if rule.required_substrings \
                         and (target_line_trimmed_len < rule.min_substrings_len
-                             or not self._check_substrings(rule.required_substrings, target_line_trimmed_lower)):
+                             or not self._check_substrings(required_substrings, target_line_trimmed_lower)):
                     continue
                 new_credential = scanner.run(self.config, rule, target)
                 if new_credential:
