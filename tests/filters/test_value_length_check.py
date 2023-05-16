732d04d89d941d9491a4a1558da5ee6cbb659897
Roman Babenko <babenek@gmail.com>
2023-05-17 01:08:41 +0300

    test improvements

--- tests/filters/test_value_length_check.py
+++ tests/filters/test_value_length_check.py
@@ -1,6 +1,7 @@
 import pytest
 
 from credsweeper.filters import ValueLengthCheck
+from tests.filters.conftest import LINE_VALUE_PATTERN
 from tests.test_utils.dummy_line_data import get_line_data
 
 
@@ -8,13 +9,13 @@ class TestValueLengthCheck:
 
     @pytest.mark.parametrize("min_len", [4])
     def test_value_length_check_p(self, file_path: pytest.fixture, success_line: pytest.fixture, min_len: int) -> None:
-        line_data = get_line_data(file_path, line=success_line, pattern=r"(?P<value>.*$)")
+        line_data = get_line_data(file_path, line=success_line, pattern=LINE_VALUE_PATTERN)
         assert ValueLengthCheck(min_len).run(line_data) is False
 
     @pytest.mark.parametrize("line", ["Cra"])
     @pytest.mark.parametrize("min_len", [4])
     def test_value_length_check_n(self, file_path: pytest.fixture, line: str, min_len: int) -> None:
-        line_data = get_line_data(file_path, line=line, pattern=r"(?P<value>.*$)")
+        line_data = get_line_data(file_path, line=line, pattern=LINE_VALUE_PATTERN)
         assert ValueLengthCheck(min_len).run(line_data) is True
 
     @pytest.mark.parametrize("min_len", [4])
