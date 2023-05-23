d37394e07e05d55ecfa2175d1934275b4f1cce7c
Roman Babenko <babenek@gmail.com>
2023-05-23 17:34:13 +0300

    [AzureAccessToken] 2023-05-23T17:34:13+03:00

--- credsweeper/filters/value_structured_token_check.py
+++ credsweeper/filters/value_structured_token_check.py
@@ -1,6 +1,5 @@
 import base64
 import contextlib
-import json
 
 from credsweeper.common.constants import LATIN_1
 from credsweeper.credentials import LineData
@@ -22,39 +21,14 @@ class ValueStructuredTokenCheck(Filter):
         """
         if not line_data.value:
             return True
-        value_len = len(line_data.value)
         with contextlib.suppress(Exception):
             # atlassian integer:bytes from base64
             if line_data.value.startswith("BBDC-"):
                 # Bitbucket HTTP Access Token
                 return ValueStructuredTokenCheck.check_atlassian_struct(line_data.value[5:])
-            elif line_data.value.startswith("eyJ"):
-                # Azure jwt token
-                delimiter_pos = line_data.value.find(".")
-                if -1 != delimiter_pos:
-                    return ValueStructuredTokenCheck.check_jwt_struct(line_data.value[:delimiter_pos])
-                else:
-                    return ValueStructuredTokenCheck.check_partially_jwt_struct(line_data.value[:value_len -
-                                                                                                value_len % 4])
             else:
                 # Jira / Confluence PAT token
                 return ValueStructuredTokenCheck.check_atlassian_struct(line_data.value)
-        return True
-
-    @staticmethod
-    def check_jwt_struct(value: str) -> bool:
-        """Returns False if decoded value has json structure"""
-        decoded = base64.b64decode(value)
-        if json.loads(decoded):
-            return False
-        return True
-
-    @staticmethod
-    def check_partially_jwt_struct(value: str) -> bool:
-        """Returns False if decoded value has { , : "JWT" substrings"""
-        decoded = base64.b64decode(value)
-        if b'"JWT"' in decoded and b',' in decoded and b'{' in decoded:
-            return False
         return True
 
     @staticmethod
