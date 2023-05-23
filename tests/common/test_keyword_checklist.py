4b28845c67d36b556a2d8d64c58aa22e92a234c0
Roman Babenko <babenek@gmail.com>
2023-05-23 07:34:38 +0300

    morpheme test refactored

--- tests/common/test_keyword_checklist.py
+++ tests/common/test_keyword_checklist.py
@@ -47,27 +47,27 @@ class TestKeywordChecklist(TestCase):
         morpheme_set = set(original_morpheme_list)
         optimized_morpheme_list = sorted(list(morpheme_set), key=lambda e: len(e))
 
-        optimized = False
-        while not optimized:
-            to_remove = set()
-            l = len(optimized_morpheme_list)
-            n = 0
-            for i in optimized_morpheme_list:
-                n += 1
-                if i.lower() == i and 3 <= len(i):
+        is_optimized = False
+        while not is_optimized:
+            morphemes_to_remove_set = set()
+            optimized_morpheme_list_len = len(optimized_morpheme_list)
+            idx = 0
+            for optimized_morpheme in optimized_morpheme_list:
+                idx += 1
+                if optimized_morpheme.lower() == optimized_morpheme and 3 <= len(optimized_morpheme):
                     # search in rest list whether the items have the substring
-                    m = n
-                    while m < l:
-                        if i in optimized_morpheme_list[m]:
-                            # should be removed
-                            to_remove.add(optimized_morpheme_list[m])
-                        m += 1
+                    local_idx = idx
+                    while local_idx < optimized_morpheme_list_len:
+                        if optimized_morpheme in optimized_morpheme_list[local_idx]:
+                            # longer morpheme should be removed because it includes short morpheme inside
+                            morphemes_to_remove_set.add(optimized_morpheme_list[local_idx])
+                        local_idx += 1
                 else:
                     # wrong - must be removed
-                    to_remove.add(i)
-            for x in to_remove:
-                optimized_morpheme_list.remove(x)
-            optimized = 0 == len(to_remove)
+                    morphemes_to_remove_set.add(optimized_morpheme)
+            for to_remove in morphemes_to_remove_set:
+                optimized_morpheme_list.remove(to_remove)
+            is_optimized = 0 == len(morphemes_to_remove_set)
         diff = original_morpheme_list_len - len(optimized_morpheme_list)
         if diff:
             with open(KeywordChecklist.MORPHEME_PATH, "w") as f:
