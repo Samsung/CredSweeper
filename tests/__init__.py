0bd449cd31cdf14d0756070dd2bc0362dcb4c8a0
Roman Babenko <babenek@gmail.com>
2023-05-24 23:37:20 +0300

    [GitlabIncomingEmailToken] 2023-05-24T23:37:20+03:00

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 
 # total number of files in test samples
-SAMPLES_FILES_COUNT: int = 100
+SAMPLES_FILES_COUNT: int = 101
 
 # credentials count after scan
-SAMPLES_CRED_COUNT: int = 97
-SAMPLES_CRED_LINE_COUNT: int = 101
+SAMPLES_CRED_COUNT: int = 98
+SAMPLES_CRED_LINE_COUNT: int = 102
 
 # credentials count after post-processing
-SAMPLES_POST_CRED_COUNT: int = 78
+SAMPLES_POST_CRED_COUNT: int = 79
 
 # archived credentials that not found without --depth
 SAMPLES_IN_DEEP_1 = 15
