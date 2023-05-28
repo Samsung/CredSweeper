f92cfd71ebaf5c6e43c21c5d237970d19a299fd7
Roman Babenko <babenek@gmail.com>
2023-05-28 17:13:55 +0300

    [skip actions] [morphlist] 2023-05-28T17:13:55+03:00

--- credsweeper/filters/group/token_base32.py
+++ credsweeper/filters/group/token_base32.py
@@ -1,13 +1,12 @@
 from credsweeper.common.constants import GroupType
 from credsweeper.config import Config
-from credsweeper.filters import ValueCoupleKeywordCheck
+from credsweeper.filters import ValueCoupleKeywordCheck, ValueTokenBase32Check
 from credsweeper.filters.group import Group
-from credsweeper.filters.value_token_base32_check import ValueTokenBase32Check
 
 
 class TokenBase32(Group):
     """TokenBase32 - uses specific for base32 entropy validation"""
 
     def __init__(self, config: Config) -> None:
-        super().__init__(config, GroupType.PATTERN)
-        self.filters.extend([ValueCoupleKeywordCheck(), ValueTokenBase32Check()])
+        super().__init__(config, GroupType.DEFAULT)
+        self.filters = [ValueCoupleKeywordCheck(), ValueTokenBase32Check()]
