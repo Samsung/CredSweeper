f21af9a4eaafd19cbbcb25f3e0091ec9e57c7d9b
Roman Babenko <babenek@gmail.com>
2023-06-12 12:11:36 +0300

    [GitlabIncomingEmailToken] fix benchmark scores

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # total number of files in test samples
-SAMPLES_FILES_COUNT: int = 102
+SAMPLES_FILES_COUNT: int = 101
 
 # credentials count after scan
 SAMPLES_CRED_COUNT: int = 97
