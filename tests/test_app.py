a7e21cd7a9b1b329d6ac0e25bcb141f209c23e62
ShinHyung Choi <sh519.choi@samsung.com>
2023-06-12 17:53:57 +0900

    Fix wrong rules and samples (#349)
    * Fix wrong rules and samples
    
    There were some rules and samples that couldn't working properly.
    Some rules are not detected in all time(DB).
    
    * Update aws_mws_key sample
    
    * Update regex of AWS MWs Key rule
    
    * Fix tests
    
    * Update benchmark.txt
    
    * Fix wrong number

--- tests/test_app.py
+++ tests/test_app.py
@@ -581,8 +581,6 @@ class TestApp(TestCase):
             rules = Util.yaml_load(APP_PATH / "rules" / "config.yaml")
             rules_set = set([i["name"] for i in rules])
             missed = {  #
-                'AWS MWS Key', 'Facebook Access Token', 'Google Multi', 'Instagram Access Token', 'MailChimp API Key',
-                'PayPal Braintree Access Token', 'Picatic API Key', 'SendGrid API Key'
             }
             self.assertSetEqual(rules_set.difference(missed), report_set, f"\n{_stdout}")
             self.assertEqual(SAMPLES_POST_CRED_COUNT, len(report))
