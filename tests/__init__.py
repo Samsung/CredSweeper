dc14ef9e6f0b8dde48bd035d9fbe7dc12a5c80a0
Roman Babenko <babenek@gmail.com>
2023-06-16 09:23:04 +0300

    data test refactored 2023-06-16T09:23:04+03:00

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,12 +10,13 @@ SAMPLES_CRED_LINE_COUNT: int = 102
 # credentials count after post-processing
 SAMPLES_POST_CRED_COUNT: int = 91
 
+# with option --doc
+SAMPLES_IN_DOC = 67
+
 # archived credentials that not found without --depth
-SAMPLES_IN_DEEP_1 = 15
+SAMPLES_IN_DEEP_1 = SAMPLES_POST_CRED_COUNT + 15
 SAMPLES_IN_DEEP_2 = SAMPLES_IN_DEEP_1 + 7
 SAMPLES_IN_DEEP_3 = SAMPLES_IN_DEEP_2 + 1
-
-SAMPLES_FILTERED_BY_POST_COUNT = 0
 
 # well known string with all latin letters
 AZ_DATA = b"The quick brown fox jumps over the lazy dog"
