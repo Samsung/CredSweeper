4f5e1c94fc79a06be596a00d775a540f2de8b271
Roman Babenko <babenek@gmail.com>
2023-06-23 10:34:59 +0300

    PEM certificate was added to doc

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,7 +11,7 @@ SAMPLES_CRED_LINE_COUNT: int = 105
 SAMPLES_POST_CRED_COUNT: int = 95
 
 # with option --doc
-SAMPLES_IN_DOC = 71
+SAMPLES_IN_DOC = 72
 
 # archived credentials that are not found without --depth
 SAMPLES_IN_DEEP_1 = SAMPLES_POST_CRED_COUNT + 17
