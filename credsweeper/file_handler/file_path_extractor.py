515d3ac392bd8312b3c1fc15a17074bba08ccc47
sh519-choi <sh519.choi@samsung.com>
2023-07-07 16:55:19 +0900

    Add lower_path to check_exclude_file() for exclude exact file path

--- credsweeper/file_handler/file_path_extractor.py
+++ credsweeper/file_handler/file_path_extractor.py
@@ -127,11 +127,12 @@ class FilePathExtractor:
         Return:
             True when the file full path should be excluded according config
         """
-        path = path.replace('\\', '/').lower()
-        if config.not_allowed_path_pattern.match(path):
+        path = path.replace('\\', '/')
+        lower_path = path.lower()
+        if config.not_allowed_path_pattern.match(lower_path):
             return True
         for exclude_pattern in config.exclude_patterns:
-            if exclude_pattern.match(path):
+            if exclude_pattern.match(lower_path):
                 return True
         for exclude_path in config.exclude_paths:
             if exclude_path in path:
