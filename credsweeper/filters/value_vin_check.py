bc614514b567622c75e2ba706509457070551578
Roman Babenko <babenek@gmail.com>
2023-08-03 15:26:00 +0300

    codestyle

--- credsweeper/filters/value_vin_check.py
+++ credsweeper/filters/value_vin_check.py
@@ -10,12 +10,41 @@ class ValueVinCheck(Filter):
     """Check that value is a VIN"""
     WEIGHTS = [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2]
     TRANSLITERATIONS = {
-        "0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
-        "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "J": 1, "K": 2,
-        "L": 3, "M": 4, "N": 5, "P": 7, "R": 9, "S": 2, "T": 3, "U": 4, "V": 5, "W": 6,
-        "X": 7, "Y": 8, "Z": 9,
+        "0": 0,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "A": 1,
+        "B": 2,
+        "C": 3,
+        "D": 4,
+        "E": 5,
+        "F": 6,
+        "G": 7,
+        "H": 8,
+        "J": 1,
+        "K": 2,
+        "L": 3,
+        "M": 4,
+        "N": 5,
+        "P": 7,
+        "R": 9,
+        "S": 2,
+        "T": 3,
+        "U": 4,
+        "V": 5,
+        "W": 6,
+        "X": 7,
+        "Y": 8,
+        "Z": 9,
     }
-    
+
     def __init__(self, config: Config = None) -> None:
         pass
 
@@ -44,9 +73,10 @@ class ValueVinCheck(Filter):
             for w, v in zip(ValueVinCheck.WEIGHTS, line_data.value):
                 s += w * ValueVinCheck.TRANSLITERATIONS[v]
             r = s % 11
-            if "X" == line_data.value[8] and 10 == r:
+            c = line_data.value[8]
+            if "X" == c and 10 == r:
                 return False
-            elif int(line_data.value[8]) == r:
+            elif ValueVinCheck.TRANSLITERATIONS[c] == r:
                 return False
 
         # return False when the sequence has wrong check digit
