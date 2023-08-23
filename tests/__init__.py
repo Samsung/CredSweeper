21bef6dcf4001b8a8afaaa001a6c7f1004772baf
Roman Babenko <babenek@users.noreply.github.com>
2023-08-23 13:51:28 +0300

    Phone number detection (#409)
    * [skip actions] [phone] 2023-08-16T10:14:11+03:00
    
    * [skip actions] [phone] 2023-08-16T10:57:52+03:00
    
    * [skip actions] [phone] 2023-08-16T12:22:56+03:00
    
    * [skip actions] [phone] 2023-08-16T12:41:28+03:00
    
    * Check phone number with length
    
    * custom_benchmark
    
    * fix test samples
    
    * fix test
    
    * fix benchmark
    
    * Update .github/workflows/benchmark.yml

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 
 # total number of files in test samples
-SAMPLES_FILES_COUNT: int = 108
+SAMPLES_FILES_COUNT: int = 109
 
 # credentials count after scan
-SAMPLES_CRED_COUNT: int = 108
-SAMPLES_CRED_LINE_COUNT: int = 119
+SAMPLES_CRED_COUNT: int = 112
+SAMPLES_CRED_LINE_COUNT: int = 123
 
 # credentials count after post-processing
-SAMPLES_POST_CRED_COUNT: int = 102
+SAMPLES_POST_CRED_COUNT: int = 106
 
 # with option --doc
 SAMPLES_IN_DOC = 72
