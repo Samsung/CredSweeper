fac78b6d71509b80e9ea292a7284129718ab6076
Roman Babenko <babenek@gmail.com>
2023-10-19 10:49:33 +0300

    fix samples count

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # total number of files in test samples
-SAMPLES_FILES_COUNT: int = 121
+SAMPLES_FILES_COUNT: int = 120
 
 # credentials count after scan
 SAMPLES_CRED_COUNT: int = 383
