6abd762d116ae77a65ecf9d744ee0efd3ec9f567
Roman Babenko <babenek@gmail.com>
2023-11-01 11:42:18 +0200

    testfix

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,7 @@ SAMPLES_FILES_COUNT: int = 120
 
 # credentials count after scan
 SAMPLES_CRED_COUNT: int = 382
-SAMPLES_CRED_LINE_COUNT: int = 402
+SAMPLES_CRED_LINE_COUNT: int = 401
 
 # credentials count after post-processing
 SAMPLES_POST_CRED_COUNT: int = 296
