b4fd6cd7d3aeb08cd6ab67d6907485d8fc83f404
Roman Babenko <babenek@gmail.com>
2023-12-03 13:52:18 +0200

    + UT for coverage

--- tests/filters/test_value_base64_data_check.py
+++ tests/filters/test_value_base64_data_check.py
@@ -12,7 +12,7 @@ class TestValueBase64DataCheck:
         line_data = get_line_data(file_path, line=line, pattern=LINE_VALUE_PATTERN)
         assert ValueBase64DataCheck().run(line_data, DUMMY_ANALYSIS_TARGET) is False
 
-    @pytest.mark.parametrize("line", ["eyJ0eXAiOiJKV1QiLC", "2AA219GG746F88F6DDA0D852A0FD3211"])
+    @pytest.mark.parametrize("line", ["eyJ0eXAiOiJKV1QiLC", "2AA219GG746F88F6DDA0D852A0FD3211", "!@#$%^&*(_)0aA"])
     def test_value_entropy_check_n(self, file_path: pytest.fixture, line: str) -> None:
         line_data = get_line_data(file_path, line=line, pattern=LINE_VALUE_PATTERN)
         assert ValueBase64DataCheck().run(line_data, DUMMY_ANALYSIS_TARGET) is True
