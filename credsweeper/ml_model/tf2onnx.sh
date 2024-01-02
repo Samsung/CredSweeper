de633d679161341451141aa21ccc713fc50ce6f7
Roman Babenko <babenek@gmail.com>
2024-01-02 16:35:30 +0200

    description added

--- credsweeper/ml_model/tf2onnx.sh
+++ credsweeper/ml_model/tf2onnx.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+# tensorflow model may be obtained from commit be06d6059f0def4f0fdb50444c08db4ce542173e
+# use virtual environment and the requirements.txt
+# python -m venv .venv
+# . .venv/bin/activate
+# python -m pip install --upgrade pip
+# python -m pip install --requirement requirements.txt
+
 python -c 'import tensorflow as tf;model=tf.keras.models.load_model("ml_model.h5");model.save("ml_model")'
 python -m tf2onnx.convert --saved-model ml_model --output ml_model.onnx --verbose --rename-inputs feature_input,line_input
 md5sum --binary ml_model.onnx >ml_model.onnx.md5
