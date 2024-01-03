93da2ed7a1553b4496e8b6c637f02389c2682c1b
Roman Babenko <babenek@gmail.com>
2024-01-03 12:16:16 +0200

    move script to experiment

--- experiment/tf2onnx/tf2onnx.sh
+++ experiment/tf2onnx/tf2onnx.sh
@@ -3,12 +3,15 @@
 set -e
 
 # tensorflow model may be obtained like this: git restore -s be06d6059f0def4f0fdb50444c08db4ce542173e -- ml_model.h5
-# use virtual environment and the requirements.txt
+# use virtual environment and the requirements.txt - there are very specific luke combination of packages verions
 # python -m venv .venv
 # . .venv/bin/activate
 # python -m pip install --upgrade pip
 # python -m pip install --requirement requirements.txt
 
+# [optional] thransform model form h5 to saved directory
 python -c 'import tensorflow as tf;model=tf.keras.models.load_model("ml_model.h5");model.save("ml_model")'
+# transform the model
 python -m tf2onnx.convert --saved-model ml_model --output ml_model.onnx --verbose --rename-inputs feature_input,line_input
-md5sum --binary ml_model.onnx >ml_model.onnx.md5
+# md5sum for integrity
+md5sum --binary ml_model.onnx
