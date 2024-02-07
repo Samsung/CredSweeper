55c44dc58ca471d86d752e338cdcf26043579394
Roman Babenko <babenek@gmail.com>
2024-02-07 08:37:12 +0200

    url cred fix

--- tests/samples/url_cred.js
+++ tests/samples/url_cred.js
@@ -1,4 +1,4 @@
-connection_url: 'dbconnection://admin:5WdF4f2jE76a@db-host-local',
+connection_url: 'dbconnection://ad%6Din:5WdF4f2jE76a@db-host-local',
 // note:dummyuser@example.com
 
-// http://no.host.real/any/path/to/nowhere/admin:f45VgF4jX79o@anydata.com
+// "fp://no.host.real/any/path/to/nowhere/","key":"f45VgF8jX79o@anydata.com"
