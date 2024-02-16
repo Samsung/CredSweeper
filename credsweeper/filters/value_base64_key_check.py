fa626ddac7aff1328204dc115abb10b26d2783cc
Roman Babenko <babenek@gmail.com>
2024-02-16 12:56:50 +0200

    Pattern MII for private key by value

---
+++ credsweeper/filters/value_base64_key_check.py
@@ -0,0 +1,56 @@
+import contextlib
+import string
+
+from cryptography.hazmat.primitives import serialization
+
+from credsweeper.config import Config
+from credsweeper.credentials import LineData
+from credsweeper.file_handler.analysis_target import AnalysisTarget
+from credsweeper.filters import Filter
+from credsweeper.utils import Util
+
+
+class ValueBase64KeyCheck(Filter):
+    """Check that candidate contains base64 encoded private key"""
+
+    def __init__(self, config: Config = None) -> None:
+        self.config = config
+
+    def run(self, line_data: LineData, target: AnalysisTarget) -> bool:
+        """Run filter checks on received token which might be structured.
+
+        Args:
+            line_data: credential candidate data
+            target: multiline target from which line data was obtained
+
+        Return:
+            True, if need to filter candidate and False if left
+
+        """
+
+        if not line_data.value:
+            return True
+        with contextlib.suppress(Exception):
+            text = line_data.value
+            # replace to space any escaped sequence except space from string.whitespace
+            for x in ["\\t", "\\n", "\\r", "\\v", "\\f"]:
+                text = text.replace(x, ' ')
+            for x in string.whitespace:
+                text = text.replace(x, '')
+            # clean sequence concatenation case:
+            text = text.replace("'+'", '')
+            text = text.replace('"+"', '')
+            # possibly url based escaping:
+            text = text.replace('%2B', '+')
+            text = text.replace('%2F', '/')
+            text = text.replace('%3D', '=')
+            # clean any other chars which should not appear
+            for x in ["'", '"', '\\', ',']:
+                text = text.replace(x, "")
+            # only PEM standard encoding supported in regex pattern to cut off ending of the key
+            key = Util.decode_base64(text, padding_safe=True, urlsafe_detect=False)
+            private_key = serialization.load_der_private_key(key, password=None)
+            if 0 < private_key.key_size:  # type: ignore
+                # access to size field check - some types have no size
+                return False
+        return True
---
+++ tests/filters/test_value_base64_key_check.py
@@ -0,0 +1,37 @@
+import re
+import unittest
+
+from credsweeper.credentials import LineData
+from credsweeper.filters import ValueBase64KeyCheck
+from tests.filters.conftest import DUMMY_ANALYSIS_TARGET
+
+
+class TestValueBase64KeyCheck(unittest.TestCase):
+    ALL_LINE_PATTERN = re.compile(
+        r"(?P<value>\bMII[A-Za-f][0-9A-Za-z/+]{8}(?s:[^!#$&()*\-.:;<=>?@\[\]^_{|}~]{8,8000}))")
+
+    def test_value_check_n(self) -> None:
+        line_data = LineData(config=None, path="dummy", file_type="", line="MIIXXXXX", info="",
+                             line_num=1, line_pos=0, pattern=TestValueBase64KeyCheck.ALL_LINE_PATTERN )
+        self.assertTrue(ValueBase64KeyCheck().run(line_data, DUMMY_ANALYSIS_TARGET))
+
+    def test_value_check_p(self) -> None:
+        line = (
+            "'''MIICXAIBAAKBgQDFgQqUYexFziVtw\\n\\rnEz9XaYjmGdpGVWAqL1NX41LIxRAy1tbo1hCBUppqWGfn"
+            "\nWHwZE5k4sYt5cE6n4hcdEPj9kdrDaeF7Te9JZg1eDE9RwWColxC+5neCBCmQY2LmWJa6+HDgDKq+DJIpx8PCDb"
+            "+i3oeOwHn00H/4T9JczK/XJzhQIDAQABAoGBALbGxu2u"
+            "+aNYNZcv/Odi4FAqY/gprYY9hmV7DcNT6L8IO8AsMTZ87mjPShv/Z"
+            "/Esl6M7Adzr+hyYjgWReTH3o6qMBdHyJhU28Ngsrt8EIaSYpE"
+            "/O 2 h d p 6 l Z vSWVt/KD+HJMRIDVqj39DphuU8f0cqJkpLoP5xcetOH"
+            "       /XVnIVQXm5AkEA4gc5HjeggbJkj4bJg/ROmKlLgMKalg0LS7Z3"
+            "\tYXIhIGJcwE27ERB5DjqMJp38Yvz8WqUchNFqF4fxSEYTWVIiMwJBAN"
+            "\t\t\t+xiNpJtX3qWI4T5iOQM7pE8Ngx+olpOpZkwQ6jBzNyTfaZZT\""
+            "XpmvzvBZKWgkvxAE3BaeHjuXKlRYp7dVTXi2cCQEeKL7rvsl66nveKmueoAO \\\\"
+            "\nRy2RH1Bkat9EBPjXOLUw7T8zVupOHey+IEB+4FHSgb"
+            "\r\n\tmKf8EI29Vo4CGg1dGKyxwV8CQAqZ/"
+            "\r   WRrDeDz1FKXetbApNL1JXz5kWglxpMOH2A0NckoZ62CQ5u0gJXwNhFmdLJor5z"
+            "/x6bfBhD66DrR8xOcS40CQBoGNvyzjHXUdx64Yxmtz2FXq0fYI8J8ChgNfTe6l6d1nzEQ7i0SspM45L"
+            "/n5tgyrBBGcqzQ/WBL1C8Ny5K+RxY='''\n  -----END")
+        line_data = LineData(config=None, path="dummy", file_type="", line=line, info="",
+                             line_num=1, line_pos=0, pattern=TestValueBase64KeyCheck.ALL_LINE_PATTERN)
+        self.assertFalse(ValueBase64KeyCheck().run(line_data, DUMMY_ANALYSIS_TARGET))
