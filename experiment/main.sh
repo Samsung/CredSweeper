932d97059c395f41396b8c5773b233b29d887cfc
Roman Babenko <babenek@gmail.com>
2024-05-20 18:01:16 +0300

    [skip actions] [ml] 2024-05-20T18:01:16+03:00

--- experiment/main.sh
+++ experiment/main.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 CREDSWEEPER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." > /dev/null 2>&1 && pwd )"
 export PYTHONPATH=${CREDSWEEPER_DIR}:$PYTHONPATH
 echo $PYTHONPATH
 ${CREDSWEEPER_DIR}/.venv/bin/python -m credsweeper --banner
 
-rm -rf data
+# rm -rf data
 
 ${CREDSWEEPER_DIR}/.venv/bin/python main.py --data ~/w/CredData --jobs 32 | tee train.log
 
