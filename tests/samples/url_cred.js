5581d1a5b3c90c37fc35af81614eb65c9d167689
Roman Babenko <babenek@gmail.com>
2024-06-10 16:07:37 +0300

    [skip actions] [auxiliary] 2024-06-10T16:07:37+03:00

--- tests/samples/url_cred.js
+++ tests/samples/url_cred.js
@@ -1,5 +1,7 @@
 const connection_url = require('dbconnection://ad%6Din:5WdF4f2jE76a@db-host-local');
 
+url = "https://secure-bucket.s3.amazonaws.com/12736/39084?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA546DFS64NP3AW7DX%2Farj";
+
 // note:dummyuser@example.com
 
 // "fp://no.host.real/any/path/to/nowhere/","key":"f45VgF8jX79o@anydata.com"
