f9b73b31cae2852dc069f56c1f8ab90c512d23f2
Roman Babenko <babenek@users.noreply.github.com>
2024-06-13 10:00:52 +0300

    URL sanitizer (#563)
    * [skip actions] [auxiliary] 2024-06-10T16:07:37+03:00
    
    * url sanitizer
    
    * custom BM, fix url sanitizer
    
    * skip variable for url cred
    
    * skip sanitize in case of URL credential rule
    
    * BM upd
    
    * lastfix[no ci]
    
    * Apply suggestions from code review

--- tests/samples/url_cred.js
+++ tests/samples/url_cred.js
@@ -1,5 +1,7 @@
 const connection_url = require('dbconnection://ad%6Din:5WdF4f2jE76a@db-host-local');
 
+url = "https://secure.com/83675/39084?Credential=546DFS64N90P3AW7DX%2Fdummyftail";
+
 // note:dummyuser@example.com
 
 // "fp://no.host.real/any/path/to/nowhere/","key":"f45VgF8jX79o@anydata.com"
