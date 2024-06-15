7cbca73a354a333872959fe410e6c4a220e73601
Roman Babenko <babenek@gmail.com>
2024-06-15 19:03:02 +0300

    ip filter true positives

--- credsweeper/filters/value_ip_check.py
+++ credsweeper/filters/value_ip_check.py
@@ -12,8 +12,11 @@ from credsweeper.utils import Util
 class ValueIPCheck(Filter):
     """Filter out some of insensible IP"""
 
+    TRUE_POSITIVE_MARKERS = ["ip", "server", "addr", "login"]
+    TRUE_POSITIVE_PATTERN = re.compile(Util.get_regex_combine_or(TRUE_POSITIVE_MARKERS), flags=re.IGNORECASE)
+
     FALSE_POSITIVE_MARKERS = ["version", "oid", "section", "rfc"]
-    FALSE_POSITIVE_PATTERN = re.compile(Util.get_regex_combine_or(FALSE_POSITIVE_MARKERS))
+    FALSE_POSITIVE_PATTERN = re.compile(Util.get_regex_combine_or(FALSE_POSITIVE_MARKERS), flags=re.IGNORECASE)
 
     def __init__(self, config: Config = None) -> None:
         pass
@@ -36,10 +39,11 @@ class ValueIPCheck(Filter):
             ip = ipaddress.ip_address(line_data.value)
             if 4 == ip.version:
                 byte_sum = sum(x for x in ip.packed)
-                if 10 > (byte_sum >> 2):
+                if 100 > (byte_sum >> 2):
                     # versions usually have low average of sum the bytes
                     search_text = Util.subtext(line_data.line, line_data.value_start, 80)
-                    if self.FALSE_POSITIVE_PATTERN.search(search_text):
+                    if self.FALSE_POSITIVE_PATTERN.search(search_text) \
+                            and not self.TRUE_POSITIVE_PATTERN.search(search_text):
                         return True
             if ip.is_loopback or ip.is_private or ip.is_reserved or ip.is_link_local or ip.is_multicast:
                 return True
