8062471ed72298e8852f0c424b2a52a0ec55c685
Roman Babenko <babenek@gmail.com>
2024-08-01 17:49:53 +0300

    hashicorp terraform token

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,20 +1,20 @@
 from pathlib import Path
 
 # total number of files in test samples
-SAMPLES_FILES_COUNT: int = 130
+SAMPLES_FILES_COUNT: int = 131
 
 # the lowest value of ML threshold is used to display possible lowest values
 NEGLIGIBLE_ML_THRESHOLD = 0.0001
 
 # credentials count after scan
-SAMPLES_CRED_COUNT: int = 429
-SAMPLES_CRED_LINE_COUNT: int = 446
+SAMPLES_CRED_COUNT: int = 430
+SAMPLES_CRED_LINE_COUNT: int = 447
 
 # credentials count after post-processing
-SAMPLES_POST_CRED_COUNT: int = 387
+SAMPLES_POST_CRED_COUNT: int = 388
 
 # with option --doc
-SAMPLES_IN_DOC = 410
+SAMPLES_IN_DOC = 411
 
 # archived credentials that are not found without --depth
 SAMPLES_IN_DEEP_1 = SAMPLES_POST_CRED_COUNT + 25
