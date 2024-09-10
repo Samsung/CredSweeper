9f676cd9e7e054ac802ff60c39b45f01b456ade7
Roman Babenko <babenek@gmail.com>
2024-09-10 13:15:00 +0300

    fix

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,20 +1,20 @@
 from pathlib import Path
 
 # total number of files in test samples
-SAMPLES_FILES_COUNT: int = 132
+SAMPLES_FILES_COUNT: int = 133
 
 # the lowest value of ML threshold is used to display possible lowest values
 NEGLIGIBLE_ML_THRESHOLD = 0.0001
 
 # credentials count after scan
-SAMPLES_CRED_COUNT: int = 380
-SAMPLES_CRED_LINE_COUNT: int = 397
+SAMPLES_CRED_COUNT: int = 386
+SAMPLES_CRED_LINE_COUNT: int = 404
 
 # credentials count after post-processing
-SAMPLES_POST_CRED_COUNT: int = 349
+SAMPLES_POST_CRED_COUNT: int = 355
 
 # with option --doc
-SAMPLES_IN_DOC = 419
+SAMPLES_IN_DOC = 423
 
 # archived credentials that are not found without --depth
 SAMPLES_IN_DEEP_1 = SAMPLES_POST_CRED_COUNT + 23
