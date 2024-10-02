92cbe275a1a33ffbe29df7286513cc6db9d6c9c1
Roman Babenko <babenek@gmail.com>
2024-10-02 14:18:43 +0300

    tests

--- credsweeper/ml_model/features/morpheme_dense.py
+++ credsweeper/ml_model/features/morpheme_dense.py
@@ -3,7 +3,7 @@ from typing import Dict, Set
 
 import numpy as np
 
-from credsweeper.common import KeywordChecklist
+from credsweeper.common import KeywordChecklist, static_keyword_checklist
 from credsweeper.common.constants import Base, Chars
 from credsweeper.credentials import Candidate
 from credsweeper.ml_model.features.feature import Feature
@@ -12,22 +12,18 @@ from credsweeper.ml_model.features.feature import Feature
 class MorphemeDense(Feature):
     """Feature calculates morphemes density for a value"""
 
-    def __init__(self, base: str) -> None:
-        """CharSet class initializer.
-
-        Args:
-            base: base set ID
-
-        """
+    def __init__(self) -> None:
+        """Class initializer"""
         super().__init__()
 
     def extract(self, candidate: Candidate) -> float:
         if value := candidate.line_data_list[0].value.lower():
             morphemes_counter = 0
-            for morpheme in KeywordChecklist.morpheme_set:
+            for morpheme in static_keyword_checklist.morpheme_set:
                 if morpheme in value:
                     morphemes_counter += 1
-            return morphemes_counter / len(value)
+            # normalization: minimal morpheme length is 3
+            return 3.0 * morphemes_counter / len(value)
         else:
             # empty value case
             return 0.0
