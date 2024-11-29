ef54041c41a73d40a255ec93e3388ab7f67dee0e
Roman Babenko <babenek@gmail.com>
2024-11-29 18:17:57 +0200

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,6 +75,8 @@ source_suffix = {
     '.md': 'markdown',
 }
 
+myst_heading_anchors = 3
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
