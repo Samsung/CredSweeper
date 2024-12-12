e5a4ee8f21689eee6b1cbef7fe19609866fc2c85
Roman Babenko <babenek@users.noreply.github.com>
2024-12-12 11:38:23 +0200

    fix \r in xlsx (#631)
    * fix \r in xlsx
    
    * Update credsweeper/filters/value_discord_bot_check.py

--- credsweeper/filters/value_discord_bot_check.py
+++ credsweeper/filters/value_discord_bot_check.py
@@ -1,9 +1,11 @@
 import contextlib
 
+from credsweeper.common.constants import Chars
 from credsweeper.config import Config
 from credsweeper.credentials import LineData
 from credsweeper.file_handler.analysis_target import AnalysisTarget
 from credsweeper.filters import Filter
+from credsweeper.filters.value_entropy_base64_check import ValueEntropyBase64Check
 from credsweeper.utils import Util
 
 
@@ -28,6 +30,10 @@ class ValueDiscordBotCheck(Filter):
             # . must be in value according regex
             dot_separator_index = line_data.value.index('.')
             id_part = line_data.value[:dot_separator_index]
-            if int(Util.decode_base64(id_part, padding_safe=True, urlsafe_detect=True)):
+            discord_id = int(Util.decode_base64(id_part, padding_safe=True, urlsafe_detect=True))
+            entropy_part = line_data.value[dot_separator_index:]
+            entropy = Util.get_shannon_entropy(entropy_part, Chars.BASE64STD_CHARS.value)
+            min_entropy = ValueEntropyBase64Check.get_min_data_entropy(len(line_data.value))
+            if not 1000 > discord_id and not min_entropy < entropy:
                 return False
         return True
