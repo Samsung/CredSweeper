85b9c2ef89a2b1810eb1e0d4c69ad34d12a3133e
Roman Babenko <babenek@gmail.com>
2024-12-13 12:49:41 +0200

    BM&test

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,7 +7,7 @@ SAMPLES_FILES_COUNT = 138
 NEGLIGIBLE_ML_THRESHOLD = 0.0001
 
 # credentials count after scan with negligible ML threshold
-SAMPLES_CRED_COUNT = 423
+SAMPLES_CRED_COUNT = 425
 SAMPLES_CRED_LINE_COUNT = SAMPLES_CRED_COUNT + 19
 
 # Number of filtered credentials with ML
@@ -17,7 +17,7 @@ ML_FILTERED = 43
 SAMPLES_POST_CRED_COUNT = SAMPLES_CRED_COUNT - ML_FILTERED
 
 # with option --doc
-SAMPLES_IN_DOC = 466
+SAMPLES_IN_DOC = 468
 
 # archived credentials that are not found without --depth
 SAMPLES_IN_DEEP_1 = SAMPLES_POST_CRED_COUNT + 33
