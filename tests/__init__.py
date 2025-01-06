4a2e95848e0245e6855e27679cfab6a6b1bc7ee5
Roman Babenko <babenek@users.noreply.github.com>
2025-01-06 17:35:54 +0200

    Discord Webhook pattern (#648)
    * salesforce
    
    * benchmark
    
    * skip json check for last symbol 0x0A
    
    * style
    
    * rollbackBM
    
    * discord webhook

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
 # total number of files in test samples
-SAMPLES_FILES_COUNT = 143
+SAMPLES_FILES_COUNT = 144
 
 # the lowest value of ML threshold is used to display possible lowest values
 NEGLIGIBLE_ML_THRESHOLD = 0.0001
 
 # credentials count after scan with negligible ML threshold
-SAMPLES_CRED_COUNT = 428
+SAMPLES_CRED_COUNT = 429
 SAMPLES_CRED_LINE_COUNT = SAMPLES_CRED_COUNT + 19
 
 # Number of filtered credentials with ML
@@ -17,7 +17,7 @@ ML_FILTERED = 32
 SAMPLES_POST_CRED_COUNT = SAMPLES_CRED_COUNT - ML_FILTERED
 
 # with option --doc
-SAMPLES_IN_DOC = 674
+SAMPLES_IN_DOC = 675
 
 # archived credentials that are not found without --depth
 SAMPLES_IN_DEEP_1 = SAMPLES_POST_CRED_COUNT + 35
