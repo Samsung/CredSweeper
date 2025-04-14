25a78b1fb45398726f01508d8fdcb648ef84413c
Roman Babenko <babenek@gmail.com>
2025-04-14 18:34:32 +0300

    custom bm

--- credsweeper/credentials/line_data.py
+++ credsweeper/credentials/line_data.py
@@ -31,7 +31,7 @@ class LineData:
     """
 
     quotation_marks = ('"', "'", '`')
-    comment_starts = ("//", "* ", "#", "/*", "<!––", "%{", "%", "...", "(*", "--", "--[[", "#=")
+    comment_starts = ("//", "* ", "#w", "/*", "<!––", "%{", "%", "...", "(*", "--", "--[[", "#=")
     bash_param_split = re.compile("\\s+(\\-|\\||\\>|\\w+?\\>|\\&)")
     line_endings = re.compile(r"\\{1,8}[nr]")
     # https://en.wikipedia.org/wiki/Percent-encoding
