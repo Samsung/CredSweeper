4d227a07cb12071629430bfb45d94154c87a9c14
Roman Babenko <babenek@gmail.com>
2025-06-02 12:12:42 +0300

    jwk

---
+++ credsweeper/filters/value_json_web_key_check.py
@@ -0,0 +1,37 @@
+import contextlib
+
+from credsweeper.config import Config
+from credsweeper.credentials import LineData
+from credsweeper.file_handler.analysis_target import AnalysisTarget
+from credsweeper.filters import Filter
+from credsweeper.utils import Util
+
+
+class ValueJsonWebKeyCheck(Filter):
+    """
+    Check that candidate is JWK which starts usually from 'e'
+    and have private parts of the key
+    https://datatracker.ietf.org/doc/html/rfc7517
+    https://datatracker.ietf.org/doc/html/rfc7518
+    """
+
+    def __init__(self, config: Config = None) -> None:
+        pass
+
+    def run(self, line_data: LineData, target: AnalysisTarget) -> bool:
+        """Run filter checks on received key which might be structured.
+
+        Args:
+            line_data: credential candidate data
+            target: multiline target from which line data was obtained
+
+        Return:
+            True, when need to filter candidate and False if left
+
+        """
+        with contextlib.suppress(Exception):
+            if data := Util.decode_base64(line_data.value, padding_safe=True, urlsafe_detect=True):
+                if b'"kty":' in data and (b'"oct"' in data and b'"k":' in data or
+                                          (b'"EC"' in data or b'"RSA"' in data) and b'"d":' in data):
+                    return False
+        return True
