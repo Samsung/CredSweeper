d723dd81ac0a40b15fa26c997e29c834819f524e
Roman Babenko <babenek@users.noreply.github.com>
2025-11-03 19:07:21 +0200

    Severity promotions for deep scanner rules (#779)
    * PKCS private keys use HIGH severity
    
    * Promote JKS rule severity
    
    * confidence fix

--- credsweeper/deep_scanner/pkcs_scanner.py
+++ credsweeper/deep_scanner/pkcs_scanner.py
@@ -3,6 +3,7 @@ import logging
 from abc import ABC
 from typing import List, Optional
 
+from credsweeper.common.constants import Severity, Confidence
 from credsweeper.credentials.candidate import Candidate
 from credsweeper.deep_scanner.abstract_scanner import AbstractScanner
 from credsweeper.file_handler.data_content_provider import DataContentProvider
@@ -35,6 +36,9 @@ class PkcsScanner(AbstractScanner, ABC):
                         "PKCS")
                     candidate.line_data_list[0].line = base64.b64encode(data_provider.data).decode()
                     candidate.line_data_list[0].value = repr(password)
+                    # high severity is assigned to private key rules
+                    candidate.severity = Severity.HIGH
+                    candidate.confidence = Confidence.STRONG
                     return [candidate]
             except Exception as pkcs_exc:
                 logger.debug(f"{data_provider.file_path}:{pw_probe}:{pkcs_exc}")
