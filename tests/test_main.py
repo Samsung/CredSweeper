07d2a1b5509adeccc8a460fc25ec7797716f7b0b
Roman Babenko <babenek@gmail.com>
2025-11-07 13:11:26 +0200

    [skip actions] [main] 2025-11-07T13:11:26+02:00

--- tests/test_main.py
+++ tests/test_main.py
@@ -349,7 +349,7 @@ class TestMain(unittest.TestCase):
         cred_sweeper = CredSweeper()
         provider = StringContentProvider([text])
         results = cred_sweeper.file_scan(provider)
-        self.assertAlmostEqual(73, len(results), delta=37)  # various lines may look like tokens
+        self.assertAlmostEqual(14, len(results), delta=7)  # various lines may look like tokens
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
