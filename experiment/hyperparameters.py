3b6ae658906f28e0ea09e91a2240ce21c59739e3
Roman Babenko <babenek@gmail.com>
2025-11-17 15:49:45 +0200

    Version Up

--- experiment/hyperparameters.py
+++ experiment/hyperparameters.py
@@ -1,10 +1,10 @@
 HP_DICT = {
-    "line_lstm_dropout_rate": ((0.3, 0.5, 0.01), 0.4),
-    "line_lstm_recurrent_dropout_rate": ((0.0, 0.4, 0.01), 0.1),
-    "variable_lstm_dropout_rate": ((0.3, 0.5, 0.01), 0.4),
-    "variable_lstm_recurrent_dropout_rate": ((0.0, 0.4, 0.01), 0.1),
-    "value_lstm_dropout_rate": ((0.3, 0.5, 0.01), 0.4),
-    "value_lstm_recurrent_dropout_rate": ((0.0, 0.4, 0.01), 0.1),
-    "dense_a_lstm_dropout_rate": ((0.1, 0.5, 0.01), 0.2),
-    "dense_b_lstm_dropout_rate": ((0.1, 0.5, 0.01), 0.2),
+    "line_lstm_dropout_rate": ((0.4, 0.5, 0.01), 0.4),
+    "line_lstm_recurrent_dropout_rate": ((0.0, 0.3, 0.01), 0.1),
+    "variable_lstm_dropout_rate": ((0.4, 0.5, 0.01), 0.4),
+    "variable_lstm_recurrent_dropout_rate": ((0.0, 0.3, 0.01), 0.1),
+    "value_lstm_dropout_rate": ((0.4, 0.5, 0.01), 0.4),
+    "value_lstm_recurrent_dropout_rate": ((0.0, 0.3, 0.01), 0.1),
+    "dense_a_drop": ((0.0, 0.3, 0.01), 0.2),
+    "dense_b_drop": ((0.0, 0.3, 0.01), 0.2),
 }
