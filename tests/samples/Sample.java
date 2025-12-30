4bb4586f2ab95a7e60994022f4106ba7214054a6
Roman Babenko <babenek@gmail.com>
2025-12-30 13:57:30 +0200

    Jclass scanner hotfix

--- tests/samples/Sample.java
+++ tests/samples/Sample.java
@@ -1,5 +1,36 @@
 import java.util.Base64;
-public class Sample {
+import java.util.function.Consumer;
+
+public class Sample implements Runnable {
+
+    private static final boolean JAVA_BOOLEAN = true;
+    private static final char JAVA_CHAR = 'X';
+    private static final byte JAVA_BYTE = 123;
+    private static final short JAWA_SHORT = 350;
+    private static final int JAVA_INT = 32768;
+    private static final long JAVA_LONG = 2000000000L;
+    private static final float JAVA_FLOAT = 3.14f;
+    private static final double JAVA_DOUBLE = 2.718281828;
+    private static final String JAVA_STRING = "bace4d19-beef-cafe-coo1-9129474bcd81";
+
+    @Override
+    public void run() {
+        Runnable r = () -> System.out.println("run");
+        r.run();
+    }
+
+    public void log() {
+        System.out.println(JAVA_BOOLEAN);
+        System.out.println(JAVA_CHAR);
+        System.out.println(JAVA_STRING);
+        System.out.println(JAVA_BYTE + JAWA_SHORT + JAVA_INT + JAVA_LONG);
+        System.out.println(JAVA_FLOAT + JAVA_DOUBLE);
+    }
+
+    public static void main(String[] args) {
+        new Sample().run();
+    }
+
     private static byte [] getPrivateKey1() {
         final byte [] pkey = Base64.getMimeDecoder().decode(
             "MIIBOgIBAAJBAL1/hJjtuMbjbVXo6wYT1SxiROOvwgffVSvOAk5aN2d4wYTC25k3"+
@@ -11,6 +42,7 @@ public class Sample {
             "LFj5jkNs4iSqphZo8eISUdol/9Zo/dkrHC41kbYJ");
         return pkey;
     }
+
     private static byte [] getPrivateKey8() {
         final String text =
             "MIIBvTBXBgkqhkiG9w0BBQ0wSjApBgkqhkiG9w0BBQwwHAQIT0gWHcAV1rACAggA\n"+
@@ -26,6 +58,7 @@ public class Sample {
         final byte [] pkey = Base64.getMimeDecoder().decode(text);
         return pkey;
     }
+
     public static byte[] get(boolean encryption){
         if (encryption) {
             return Sample.getPrivateKey8();
@@ -34,4 +67,5 @@ public class Sample {
         }
     }
 }
+
 /* javac Sample.java && javap -c -l -p -v -s Sample.class */
