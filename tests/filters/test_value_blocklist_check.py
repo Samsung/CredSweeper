a38c4bf61c16de1f76f063c56aed0b86eaa7a84a
Roman Babenko <babenek@gmail.com>
2026-02-19 15:40:05 +0200

    fix

--- tests/filters/test_value_blocklist_check.py
+++ tests/filters/test_value_blocklist_check.py
@@ -11,9 +11,7 @@ class TestValueBlocklistCheck:
         line_data = get_line_data(file_path, line=success_line, pattern=LINE_VALUE_PATTERN)
         assert ValueBlocklistCheck().run(line_data, DUMMY_ANALYSIS_TARGET) is False
 
-    @pytest.mark.parametrize("line", [
-        "string12", "uuid5"
-    ])
+    @pytest.mark.parametrize("line", ["string12", "uuid5"])
     def test_value_blocklist_n(self, file_path: pytest.fixture, line: str) -> None:
         line_data = get_line_data(file_path, line=line, pattern=LINE_VALUE_PATTERN)
         assert ValueBlocklistCheck().run(line_data, DUMMY_ANALYSIS_TARGET) is True
