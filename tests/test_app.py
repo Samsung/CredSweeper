5b40bcfafc129873a07a7007ed87c54925ece5bf
Roman Babenko <babenek@gmail.com>
2026-04-11 10:59:54 +0000

    fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -503,7 +503,7 @@ class TestApp(TestCase):
                     cvs_checksum = hashlib.md5(f.read()).digest()
                 checksum = bytes(a ^ b for a, b in zip(checksum, cvs_checksum))
         # update the checksum manually and keep line endings in the samples as is (git config core.autocrlf false)
-        self.assertEqual("d4d5ffafdbc3566fedd0ad27f4bd265e", binascii.hexlify(checksum).decode())
+        self.assertEqual("698d8c529c09aded88f8221f75fdeede", binascii.hexlify(checksum).decode())
         normal_report = []
         sorted_report = []
         with tempfile.TemporaryDirectory() as tmp_dir:
