785af158bec73f950a3687143a5b8926572e0dde
Roman Babenko <babenek@gmail.com>
2026-05-26 15:03:23 +0000

    deep-protobuf-scanner

---
+++ credsweeper/deep_scanner/protobuf_scanner.py
@@ -0,0 +1,125 @@
+import logging
+from abc import ABC
+from typing import List, Optional, Generator, Tuple
+
+from credsweeper.common.constants import MIN_DATA_LEN
+from credsweeper.credentials.candidate import Candidate
+from credsweeper.deep_scanner.abstract_scanner import AbstractScanner
+from credsweeper.file_handler.data_content_provider import DataContentProvider
+
+logger = logging.getLogger(__name__)
+
+
+class ProtobufScanner(AbstractScanner, ABC):
+    """Implements protobuf (ar) scanning"""
+
+    @staticmethod
+    def read_varint(data: bytes | bytearray, offset: int) -> Tuple[int, int]:
+        """Reads varint from offset up to 64 bit values (10 bytes)
+
+        Returns: used bytes (-1 when overflow), the value
+        """
+        data_len = len(data)
+        counter = value = shift = 0
+        for i in range(offset, offset + 10):
+            if i < data_len and 10 > counter:
+                counter += 1
+                d = data[i]
+                if 0x7F < d:
+                    value |= (0x7F & d) << shift
+                    shift += 7
+                    continue
+                value |= d << shift
+                return counter, value
+            break
+        return -1, 0
+
+    @staticmethod
+    def read_wire(data: bytes | bytearray, offset: int) -> Tuple[int, int]:
+        """Reads wire to detect sizes
+
+        Returns: size of wire type (with primitives types), size of data (length-delimited)
+        """
+        n, s = ProtobufScanner.read_varint(data, offset)
+        if 0 < n:
+            t = 0x3 & s
+            if 0 == t:
+                # varint
+                _n, _ = ProtobufScanner.read_varint(data, offset + n)
+                if 0 < _n:
+                    return n + _n, 0
+            elif 1 == t:
+                # 64 bit fixed
+                return n + 8, 0
+            elif 2 == t:
+                # length-delimited
+                _n, _s = ProtobufScanner.read_varint(data, offset + n)
+                if 0 < _n:
+                    return n + _n, _s
+            elif 3 == t or 4 == t:
+                # deprecated
+                return n, 0
+            elif 5 == t:
+                # 32 bit fixed
+                return n + 4, 0
+        return -1, 0
+
+    @staticmethod
+    def match_protobuf(data: bytes | bytearray, offset: int, limit: int) -> bool:
+        """Process data from start to end as simple protobuf chunk
+
+        Returns: True when whole chunk was utilized with protobuf structure
+        """
+        while offset < limit:
+            n, s = ProtobufScanner.read_wire(data, offset)
+            if 0 < n:
+                offset += n + s
+            else:
+                break
+        else:
+            return bool(offset == limit)
+        return False
+
+    @staticmethod
+    def match(data: bytes | bytearray) -> bool:
+        """Simple structure check for whole data"""
+        if data:
+            return ProtobufScanner.match_protobuf(data, 0, len(data))
+        return False
+
+    @staticmethod
+    def walk_protobuf(data: bytes, offset: int, limit: int) -> Generator[Tuple[int, bytes], None, None]:
+        """Processes sequence of protobuf and yields offset and data recursive"""
+        while offset < limit:
+            n, s = ProtobufScanner.read_wire(data, offset)
+            if 0 > n:
+                raise ValueError(f"Wrong data at 0x{offset:x}")
+            offset += n
+            if MIN_DATA_LEN < s:
+                # yield valuable bytes only
+                if ProtobufScanner.match_protobuf(data, offset, offset + s):
+                    yield from ProtobufScanner.walk_protobuf(data, offset, offset + s)
+                else:
+                    yield offset, data[offset:offset + s]
+            offset += s
+
+    def data_scan(
+            self,  #
+            data_provider: DataContentProvider,  #
+            depth: int,  #
+            recursive_limit_size: int) -> Optional[List[Candidate]]:
+        """Extracts data file from protobuf payload and launches data_scan"""
+        try:
+            candidates: List[Candidate] = []
+            for offset, data in ProtobufScanner.walk_protobuf(data_provider.data, 0, len(data_provider.data)):
+                provider = DataContentProvider(data=data,
+                                               file_path=data_provider.file_path,
+                                               file_type=data_provider.file_type,
+                                               info=f"{data_provider.info}|PROTO:0x{offset:x}")
+                new_limit = recursive_limit_size - len(data)
+                protobuf_candidates = self.recursive_scan(provider, depth, new_limit)
+                candidates.extend(protobuf_candidates)
+            return candidates
+        except Exception as exc:
+            logger.warning(exc)
+        return None
---
+++ tests/deep_scanner/test_protobuf_scanner.py
@@ -0,0 +1,33 @@
+import random
+import unittest
+
+from credsweeper.deep_scanner.protobuf_scanner import ProtobufScanner
+from tests import AZ_DATA
+
+
+class TestProtobufScanner(unittest.TestCase):
+
+    def test_read_varint_n(self):
+        self.assertGreater((0, 0), ProtobufScanner.read_varint(b"\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF", 0))
+        self.assertGreater((0, 0), ProtobufScanner.read_varint(b"\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF", 5))
+
+    def test_read_varint_p(self):
+        self.assertEqual((1, 16), ProtobufScanner.read_varint(b"\x10", 0))
+        self.assertEqual((1, 0), ProtobufScanner.read_varint(b"\x00", 0))
+        self.assertEqual((1, 1), ProtobufScanner.read_varint(b"\x01", 0))
+        self.assertEqual((1, 127), ProtobufScanner.read_varint(b"\x7F", 0))
+        self.assertEqual((2, 128), ProtobufScanner.read_varint(b"\x80\x01", 0))
+        self.assertEqual((2, 150), ProtobufScanner.read_varint(b"\xFF\x96\x01\xABc\xDE\xFF", 1))
+        self.assertEqual((5, 31726677630), ProtobufScanner.read_varint(b"\xFE\xDC\xBA\x98\x76\xFF", 0))
+        self.assertEqual((2, 16383), ProtobufScanner.read_varint(b"\xFF\xFF\xFF\xFF\xFF\x7F", 4))
+        self.assertEqual((3, 2097151), ProtobufScanner.read_varint(b"\xFF\xFF\xFF\xFF\xFF\x7F", 3))
+        self.assertEqual((4, 268435455), ProtobufScanner.read_varint(b"\xFF\xFF\xFF\xFF\xFF\x7F", 2))
+        self.assertEqual((5, 34359738367), ProtobufScanner.read_varint(b"\xFF\xFF\xFF\xFF\xFF\x7F", 1))
+
+    def test_match_n(self):
+        self.assertFalse(ProtobufScanner.match(AZ_DATA))
+        self.assertFalse(ProtobufScanner.match(random.randbytes(8)))
+
+    def test_match_p(self):
+        self.assertFalse(ProtobufScanner.match(AZ_DATA))
+        self.assertFalse(ProtobufScanner.match(random.randbytes(8)))
