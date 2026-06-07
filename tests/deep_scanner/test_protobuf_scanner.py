488116c0805372ef4a2ac2633fb2be1c03fdbe95
Roman Babenko <babenek@gmail.com>
2026-06-07 07:13:55 +0300

    statistical test for protobuf match

--- tests/deep_scanner/test_protobuf_scanner.py
+++ tests/deep_scanner/test_protobuf_scanner.py
@@ -1,7 +1,7 @@
 import random
 import unittest
 
-from credsweeper.common.constants import MIN_DATA_LEN
+from credsweeper.common.constants import MIN_DATA_LEN, MAX_LINE_LENGTH
 from credsweeper.deep_scanner.protobuf_scanner import ProtobufScanner
 from tests import AZ_DATA
 
@@ -14,7 +14,14 @@ class TestProtobufScanner(unittest.TestCase):
         self.assertFalse(ProtobufScanner.match(b''))
         self.assertFalse(ProtobufScanner.match(None))
         # may fail in some percent cases
-        self.assertFalse(ProtobufScanner.match(random.randbytes(MIN_DATA_LEN)))
+        fp = 0
+        for n in range(100):
+            dl = random.randint(MIN_DATA_LEN, MAX_LINE_LENGTH)
+            rd = random.randbytes(dl)
+            if ProtobufScanner.match(rd):
+                fp += 1
+        # less than 5%
+        self.assertGreater(5, fp, "Restart test in fail case")
 
     def test_match_p(self):
         self.assertTrue(ProtobufScanner.match(b"\x08\x96\x01\x12\x0BCredSweeper"))
