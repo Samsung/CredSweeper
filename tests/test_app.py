a9b4e983bd331e3057c7a04504fcab88faf8df4a
Roman Babenko <babenek@gmail.com>
2026-06-19 11:53:11 +0000

    BMfix

--- tests/test_app.py
+++ tests/test_app.py
@@ -516,7 +516,7 @@ class TestApp(TestCase):
                     cvs_checksum = hashlib.md5(f.read()).digest()
                 checksum = bytes(a ^ b for a, b in zip(checksum, cvs_checksum))
         # update the checksum manually and keep line endings in the samples as is (git config core.autocrlf false)
-        self.assertEqual("59b755176262a6419f751f91fea8b49d", binascii.hexlify(checksum).decode())
+        self.assertEqual("543caffc72ecb9e06cabdc53b7cf8bde", binascii.hexlify(checksum).decode())
         with tempfile.TemporaryDirectory() as tmp_dir:
             json_filename = os.path.join(tmp_dir, f"{__name__}.json")
             # depth = 3
@@ -607,7 +607,7 @@ class TestApp(TestCase):
             rules_text = yaml.dump_all(rules, sort_keys=True)
             checksum = hashlib.md5(rules_text.encode()).hexdigest()
             # update the expected value manually if some changes
-            self.assertEqual("c1c38b3d962fcc89ec6960886ceb938f", checksum)
+            self.assertEqual("1d3e7351884a4f6cc2a93727cc32f709", checksum)
             rules_set = set([i["name"] for i in rules if "code" in i["target"]])
             self.assertSetEqual(rules_set, report_set)
             self.assertEqual(SAMPLES_POST_CRED_COUNT, len(report))
