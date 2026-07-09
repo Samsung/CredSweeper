c056b16f4ac1913a3dc31a5aba7ca6bb805f0018
Roman Babenko <babenek@users.noreply.github.com>
2026-07-09 18:45:40 +0300

    Wundergraph API Key (#871)
    * tar synonims
    
    * WunderGraph API Key
    
    * fix
    
    * bm fix

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # total number of files in test samples
-SAMPLES_FILES_COUNT = 210
+SAMPLES_FILES_COUNT = 211
 
 # ML_DELTA for different platforms which may produce a dribbling in ml_probability
 ML_DELTA = 0.0001
@@ -10,16 +10,16 @@ ML_DELTA = 0.0001
 ZERO_ML_THRESHOLD = 0.0
 
 # with option --doc & ZERO_ML_THRESHOLD
-SAMPLES_IN_DOC = 971
+SAMPLES_IN_DOC = 972
 
 # credentials count after scan without filters and ML validations
-SAMPLES_REGEX_COUNT = 736
+SAMPLES_REGEX_COUNT = 738
 
 # credentials count after scan with filters and without ML validation
-SAMPLES_FILTERED_COUNT = 594
+SAMPLES_FILTERED_COUNT = 595
 
 # credentials count after default post-processing
-SAMPLES_POST_CRED_COUNT = 545
+SAMPLES_POST_CRED_COUNT = 546
 
 # archived credentials that are not found without --depth
 SAMPLES_IN_DEEP_1 = SAMPLES_POST_CRED_COUNT + 152
