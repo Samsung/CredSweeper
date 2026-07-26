e5385838bf8fb9dd2eb72c96a2b5d62fe4ca863a
Roman Babenko <babenek@gmail.com>
2026-07-26 12:29:32 +0300

    tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # total number of files in test samples
-SAMPLES_FILES_COUNT = 215
+SAMPLES_FILES_COUNT = 218
 
 # ML_DELTA for different platforms which may produce a dribbling in ml_probability
 ML_DELTA = 0.0001
@@ -10,16 +10,16 @@ ML_DELTA = 0.0001
 ZERO_ML_THRESHOLD = 0.0
 
 # with option --doc & ZERO_ML_THRESHOLD
-SAMPLES_IN_DOC = 973
+SAMPLES_IN_DOC = 985
 
 # credentials count after scan without filters and ML validations
-SAMPLES_REGEX_COUNT = 738
+SAMPLES_REGEX_COUNT = 755
 
 # credentials count after scan with filters and without ML validation
-SAMPLES_FILTERED_COUNT = 595
+SAMPLES_FILTERED_COUNT = 607
 
 # credentials count after default post-processing
-SAMPLES_POST_CRED_COUNT = 546
+SAMPLES_POST_CRED_COUNT = 558
 
 # archived credentials that are not found without --depth
 SAMPLES_IN_DEEP_1 = SAMPLES_POST_CRED_COUNT + 166
