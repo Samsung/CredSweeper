d6a1e3aadb41db9431abbe968787592bcd441d81
Roman Babenko <babenek@gmail.com>
2026-08-15 05:41:32 +0300

    [skip actions] [todo] 2026-08-15T05:41:32+03:00

--- tests/samples/key.php
+++ tests/samples/key.php
@@ -1,5 +1,5 @@
 $key = 'gmUGWxhQW9KQWdhd0lCQVFRZ0ViVnpwrLzZYdDJPNG1PQjYxMXNPaFJ1
 JKhbJygU7REwsRduuhhUGui6T6=='
 define( 'PASSWORD' , '2Arh5dA1Sn0ywiVYdnpwr3aLzZDJPNG' );
-'access_id'  => 'LTAIwRQQQIlUKh25',
-'access_key' => 'ChsYewaCxm1mi7AIBPRniuncEbFHNO',
+'access_id'  => 'LTAIuck0vKQhmLADiTvgmcyCKKXN',
+'access_key' => '8zBhTTWfBKWP9hJ8wunD7zMaEvWOGg',
