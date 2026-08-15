ee8455b085a6ae53a029276882c75a3555754c26
Roman Babenko <babenek@gmail.com>
2026-08-15 10:06:39 +0300

    [skip actions] [curlopt] 2026-08-15T10:06:39+03:00

--- tests/samples/sample.c
+++ tests/samples/sample.c
@@ -2,14 +2,22 @@
 
 #define CURLOPT_PROXYPASSWORD 1
 #define CURLOPT_XOAUTH2_BEARER 2
+#define CURLOPT_PASSWORD 3
+#define CURLOPT_USERPWD 4
+#define CURLOPT_PROXYUSERPWD 5
+#define CURLOPT_KEYPASSWD 6
 
-void easy_setopt(int a, int b, char*c){
-    printf("%d, %d, %s\n", a, b, c);
-    }
+void easy_setopt(void *a, int b, char*c){
+    printf("%p, %d, %s\n", a, b, c);
+}
 
 int main(int argc, char **argv){
-    int curl = 0;
-    easy_setopt(curl, CURLOPT_PROXYPASSWORD, "84c815a317ca518fb328a2fe2824528f82e12c055e0800f3cd8dc065e9ccc09b");
-    easy_setopt(curl, CURLOPT_XOAUTH2_BEARER, "fce4fbfb98b13d50adb16edfc97dcafa7d15429ec204b4c8763287aaec3adc4c");
+    void *curl = NULL;
+    easy_setopt(curl, CURLOPT_XOAUTH2_BEARER, "K5aS1fMnmhQJMRKRw75Mwm3AVuRVZT39-v0hDyAfeqOYJZ3ji9mhO4lQ_oN2aT2Juugcd5CYqTso");
+    easy_setopt(curl, CURLOPT_PASSWORD, "KiASRCRiGFLPDiIdZZLBXaNzXCMEdYIs");
+    easy_setopt(curl, CURLOPT_USERPWD, "admin:dkb82VddSD");
+    easy_setopt(curl, CURLOPT_PROXYPASSWORD, ")d!~ORI8ed7293eg@w8&()");
+    easy_setopt(curl, CURLOPT_PROXYUSERPWD, "guest:NjWbc62SosW");
+    easy_setopt(curl, CURLOPT_KEYPASSWD, "lT3t7TlhmUkNbDO4rXkmWlQ2LRymzhDKYb");
     return 0;
 }
