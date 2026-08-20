d1e44fbd6ef949fe4d35e0d5ba2f3d967dec51d6
Roman Babenko <babenek@gmail.com>
2026-08-20 03:58:12 +0000

    progress test full

--- tests/test_progress.py
+++ tests/test_progress.py
@@ -6,11 +6,15 @@ from credsweeper.progress import Progress
 class ProgressTest(unittest.TestCase):
 
     def test_callback_n(self):
-        self.assertIsNone(Progress().progress_bar)
+        p = Progress()
+        self.assertIsNone(p.progress_bar)
+        p.callback("test", 1, 9)
+        p.callback("fake", 9, 1)
+        self.assertIsNone(p.progress_bar)
 
     def test_callback_p(self):
         p = Progress()
-        p.callback("test", 1, 2)
+        p.callback("test", 1, 9)
         self.assertIsNotNone(p.progress_bar)
-        p.callback("test", 2, 2)
+        p.callback("test", 9, 9)
         self.assertIsNone(p.progress_bar)
