1a6b56dad39ad54d97832dc83b67b684d1e5ea87
Roman Babenko <babenek@gmail.com>
2026-08-22 13:48:46 +0300

    CircleCI Personal API Token

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # total number of files in test samples
-SAMPLES_FILES_COUNT = 230
+SAMPLES_FILES_COUNT = 231
 
 # ML_DELTA for different platforms which may produce a dribbling in ml_probability
 ML_DELTA = 0.0001
@@ -10,16 +10,16 @@ ML_DELTA = 0.0001
 ZERO_ML_THRESHOLD = 0.0
 
 # with option --doc & ZERO_ML_THRESHOLD
-SAMPLES_IN_DOC = 1008
+SAMPLES_IN_DOC = 1009
 
 # credentials count after scan without filters and ML validations
-SAMPLES_REGEX_COUNT = 790
+SAMPLES_REGEX_COUNT = 792
 
 # credentials count after scan with filters and without ML validation
-SAMPLES_FILTERED_COUNT = 630
+SAMPLES_FILTERED_COUNT = 631
 
 # credentials count after default post-processing
-SAMPLES_POST_CRED_COUNT = 581
+SAMPLES_POST_CRED_COUNT = 582
 
 # archived credentials that are not found without --depth
 SAMPLES_IN_DEEP_1 = SAMPLES_POST_CRED_COUNT + 177
