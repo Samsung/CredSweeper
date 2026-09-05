b592c1e085785d17d1965b4c19b5b93a59b43de6
Roman Babenko <babenek@users.noreply.github.com>
2026-09-05 16:56:39 +0300

    airtable (#935)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # total number of files in test samples
-SAMPLES_FILES_COUNT = 241
+SAMPLES_FILES_COUNT = 242
 
 # ML_DELTA for different platforms which may produce a dribbling in ml_probability
 ML_DELTA = 0.0001
@@ -10,16 +10,16 @@ ML_DELTA = 0.0001
 ZERO_ML_THRESHOLD = 0.0
 
 # with option --doc & ZERO_ML_THRESHOLD
-SAMPLES_IN_DOC = 1036
+SAMPLES_IN_DOC = 1037
 
 # credentials count after scan without filters and ML validations
-SAMPLES_REGEX_COUNT = 834
+SAMPLES_REGEX_COUNT = 836
 
 # credentials count after scan with filters and without ML validation
-SAMPLES_FILTERED_COUNT = 658
+SAMPLES_FILTERED_COUNT = 659
 
 # credentials count after default post-processing
-SAMPLES_POST_CRED_COUNT = 609
+SAMPLES_POST_CRED_COUNT = 610
 
 # archived credentials that are not found without --depth
 SAMPLES_IN_DEEP_1 = SAMPLES_POST_CRED_COUNT + 177
--- tests/test_main.py
+++ tests/test_main.py
@@ -218,7 +218,7 @@ class TestMain(unittest.TestCase):
                     cvs_checksum = hashlib.md5(f.read()).digest()
                 checksum = bytes(a ^ b for a, b in zip(checksum, cvs_checksum))
         # update the checksum manually and keep line endings in the samples as is (git config core.autocrlf false)
-        self.assertEqual("33077dfc4ec490a937f60dbeb8d1d3c4", binascii.hexlify(checksum).decode())
+        self.assertEqual("1b5be3caa8aa60e95c0348aae2fa1fda", binascii.hexlify(checksum).decode())
         with tempfile.TemporaryDirectory() as tmp_dir:
             json_filename = os.path.join(tmp_dir, f"{__name__}.json")
             # depth = 3
@@ -335,7 +335,7 @@ class TestMain(unittest.TestCase):
             rules_text = yaml.dump_all(rules, sort_keys=True)
             checksum = hashlib.md5(rules_text.encode()).hexdigest()
             # update the expected value manually if some changes
-            self.assertEqual("2e641dc1ebfa455df10b12ac20aecbb3", checksum)
+            self.assertEqual("0e1c4c046f5328160dea65ef849e7272", checksum)
             rules_set = set([i["name"] for i in rules if "code" in i["target"]])
             self.assertSetEqual(rules_set, report_set)
             self.assertEqual(SAMPLES_POST_CRED_COUNT, len(report))
