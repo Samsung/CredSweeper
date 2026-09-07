84e193ca8b64acfc7e6ef620ea26e40889232e6a
Roman Babenko <babenek@users.noreply.github.com>
2026-09-07 14:15:36 +0300

    Activation key pattern (#938)

--- tests/test_main.py
+++ tests/test_main.py
@@ -218,7 +218,7 @@ class TestMain(unittest.TestCase):
                     cvs_checksum = hashlib.md5(f.read()).digest()
                 checksum = bytes(a ^ b for a, b in zip(checksum, cvs_checksum))
         # update the checksum manually and keep line endings in the samples as is (git config core.autocrlf false)
-        self.assertEqual("1b5be3caa8aa60e95c0348aae2fa1fda", binascii.hexlify(checksum).decode())
+        self.assertEqual("cf4a277de65f2cf7a6a99abf9a8df789", binascii.hexlify(checksum).decode())
         with tempfile.TemporaryDirectory() as tmp_dir:
             json_filename = os.path.join(tmp_dir, f"{__name__}.json")
             # depth = 3
@@ -335,7 +335,7 @@ class TestMain(unittest.TestCase):
             rules_text = yaml.dump_all(rules, sort_keys=True)
             checksum = hashlib.md5(rules_text.encode()).hexdigest()
             # update the expected value manually if some changes
-            self.assertEqual("0e1c4c046f5328160dea65ef849e7272", checksum)
+            self.assertEqual("9fd419ab7b9535b8cc1d0bdbde7a901b", checksum)
             rules_set = set([i["name"] for i in rules if "code" in i["target"]])
             self.assertSetEqual(rules_set, report_set)
             self.assertEqual(SAMPLES_POST_CRED_COUNT, len(report))
